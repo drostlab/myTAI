41fbe24424515f00b620112b0d6ece25b07b2caa
HajkD <hajkdrost@googlemail.com>
2015-05-07 15:40:53 +0200

    implementing new methods : "min-set" and "n-set" in FilterRNASeqCT() allowing users to remove genes from a count table based on stage wise expression

--- R/FilterRNASeqCT.R
+++ R/FilterRNASeqCT.R
@@ -8,11 +8,42 @@
 #' @param n a numeric value for \code{method = "n-set"}.
 #' @author Hajk-Georg Drost
 #' @details
+#' This filter function allows users to remove genes from the \code{ExpressionSet} object that undercut a certain expression level \code{cut.off}.
 #' 
-#' The
+#' Following extraction criteria are implemented in this function: 
+#' \itemize{
+#' \item \code{const}: all genes that have at least one stage that undercuts the expression \code{cut.off} will be excluded from the \code{ExpressionSet}.
+#' \item \code{min-set}: genes that undercut the expression level \code{cut.off} in \code{ceiling(n/2)} stages will be excluded from the \code{ExpressionSet}.
+#' \item \code{n-set}: genes that undercut the expression level \code{cut.off} in \code{n} stages will be excluded from the \code{ExpressionSet}. 
+#' }
+#' 
 #' @examples
+#' data(PhyloExpressionSetExample)
 #' 
-#' # 
+#' # remove genes that have an expression level below 8000 
+#' # in at least one developmental stage
+#' FilterConst <- FilterRNASeqCT(ExpressionSet = PhyloExpressionSetExample, 
+#'                               cut.off       = 8000, 
+#'                               method        = "const")
+#'                               
+#' dim(FilterConst) # check number of retained genes
+#' 
+#' # remove genes that have an expression level below 8000 
+#' # in at least ceiling(n/2) developmental stages (in this case: ceiling(7/2) = 4 stages)
+#' FilterMinSet <- FilterRNASeqCT(ExpressionSet = PhyloExpressionSetExample, 
+#'                                cut.off       = 8000, 
+#'                                method        = "min-set")
+#'                                
+#' dim(FilterMinSet) # check number of retained genes
+#' 
+#' # remove genes that have an expression level below 8000 
+#' # in at least n developmental stages (in this case: 5 stages)
+#' FilterNSet <- FilterRNASeqCT(ExpressionSet = PhyloExpressionSetExample, 
+#'                              cut.off       = 8000, 
+#'                              method        = "n-set",
+#'                              n             = 5)
+#'                                
+#' dim(FilterMinSet) # check number of retained genes
 #' 
 #' @export
 
@@ -29,27 +60,27 @@ FilterRNASeqCT <- function(ExpressionSet, cut.off, method = "const", n = NULL){
         }
         else if (method == "min-set"){
                 CandidateSet <- unique( unlist( apply( ExpressionSet[ , 3:ncols], 2 ,function(x) list(which(x < cut.off))) ) )
-# count for each gene how many stages are above the cutoff; aco = above cut off
-MinSet <- dplyr::summarise(dplyr::group_by(ExpressionSet[CandidateSet, ], GeneID), aco = function(x) sum(which(x > cutoff)))
-MinSet.Filtered <- dplyr::filter(MinSet, aco <= ceiling((ncols-2)/2))
-NEGs <- as.vector(dplyr::select(MinSet.Filtered, GeneID))
+                # count for each gene how many stages are above the cutoff; aco = above cut off
+                MinSet <- apply(ExpressionSet[CandidateSet, 3:ncols],1,function(x) sum(x > cut.off))
+                NEGs <- which(MinSet <= ceiling((ncols-2)/2))
         } 
-else if (method == "n-set"){
+        else if (method == "n-set"){
+                if(is.null(n))
+                        stop("Please specify the number of stages n for which expresssion levels need to be above the cutoff to be retained in the count table.")
         
-        if(is.null(n))
-                stop("Please specify the number of stages n for which expresssion levels need to be above the cutoff to be retained in the count table.")
+                        CandidateSet <- unique( unlist( apply( ExpressionSet[ , 3:ncols], 2 ,function(x) list(which(x < cut.off))) ) )
+        # count for each gene how many stages are above the cutoff; aco = above cut off
+                        MinSet <- apply(ExpressionSet[CandidateSet, 3:ncols],1,function(x) sum(x > cut.off))
+                        NEGs <- which(MinSet <= n)
+        } 
         
-        CandidateSet <- unique( unlist( apply( ExpressionSet[ , 3:ncols], 2 ,function(x) list(which(x < cut.off))) ) )
-# count for each gene how many stages are above the cutoff; aco = above cut off
-MinSet <- dplyr::summarise(dplyr::group_by(ExpressionSet[CandidateSet, ], GeneID), aco = function(x) sum(which(x > cutoff)))
-MinSet.Filtered <- dplyr::filter(MinSet, aco <= n)
-NEGs <- as.vector(dplyr::select(MinSet.Filtered, GeneID))
-} 
+        if(length(NEGs) > 1){
+                return(ExpressionSet[ -NEGs , ])
+        } else {
+                return(ExpressionSet)
+        }
+}
 
-if(length(NEGs) > 1){
-        return(ExpressionSet[ -NEGs , ])
-        
-} else {
-        return(ExpressionSet)
-}
-}
+
+
+
