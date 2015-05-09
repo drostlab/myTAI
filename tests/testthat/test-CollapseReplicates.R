00ffe1da1fd5aad9e90821f35b990370e7ad14e2
HajkD <hajkdrost@googlemail.com>
2015-05-09 14:58:26 +0200

    adding additional unit tests for CollapseReplicates()

--- tests/testthat/test-CollapseReplicates.R
+++ tests/testthat/test-CollapseReplicates.R
@@ -25,11 +25,28 @@ colnames(TestCollapsedDF3)[3:5] <- c("S1","S2","S3")
 TestCollapsedDF4 <- cbind(PhyloExpressionSetExample[1:5, 1:2],apply(PhyloExpressionSetExample[1:5, 3:4],1,max),apply(PhyloExpressionSetExample[1:5, 5:6],1,max),apply(PhyloExpressionSetExample[1:5, 7:8],1,max))
 colnames(TestCollapsedDF4)[3:5] <- c("S1","S2","S3")
 
+
+# test df: median, nrep = c(2,2,3), stage.names = c("S1","S2","S3")
+TestCollapsedDF5 <- cbind(PhyloExpressionSetExample[1:5, 1:2],apply(PhyloExpressionSetExample[1:5, 3:4],1,median),apply(PhyloExpressionSetExample[1:5, 5:6],1,median),apply(PhyloExpressionSetExample[1:5, 7:9],1,median))
+colnames(TestCollapsedDF5)[3:5] <- c("S1","S2","S3")
+
+# test df: max, nrep = c(2,2,3), stage.names = c("S1","S2","S3")
+TestCollapsedDF6 <- cbind(PhyloExpressionSetExample[1:5, 1:2],apply(PhyloExpressionSetExample[1:5, 3:4],1,max),apply(PhyloExpressionSetExample[1:5, 5:6],1,max),apply(PhyloExpressionSetExample[1:5, 7:9],1,max))
+colnames(TestCollapsedDF6)[3:5] <- c("S1","S2","S3")
+
+
 test_that("CollapseReplicates() works with const. nrep = 2 and FUN = mean", {
         
         expect_true(equal_df(CollapseReplicates(PhyloExpressionSetExample[1:5,1:8],2,mean,c("S1","S2","S3")), TestCollapsedDF1))
         
 })
+
+test_that("CollapseReplicates() works with const. nrep = 2 and FUN = 'mean' (quoted)", {
+        
+        expect_true(equal_df(CollapseReplicates(PhyloExpressionSetExample[1:5,1:8],2,"mean",c("S1","S2","S3")), TestCollapsedDF1))
+        
+})
+
 
 test_that("CollapseReplicates() works with const. nrep = 2 and FUN = median", {
         
@@ -42,9 +59,23 @@ test_that("CollapseReplicates() works with const. nrep = 2 and FUN = max", {
         expect_true(equal_df(CollapseReplicates(PhyloExpressionSetExample[1:5,1:8],2,max,c("S1","S2","S3")), TestCollapsedDF4))
         
 })
+
 test_that("CollapseReplicates() works with variable nrep = c(2,2,3) and FUN = mean", {
         
         expect_true(equal_df(CollapseReplicates(PhyloExpressionSetExample[1:5,1:9],c(2,2,3),mean,c("S1","S2","S3")), TestCollapsedDF2))
+        
+})
+
+
+test_that("CollapseReplicates() works with variable nrep = c(2,2,3) and FUN = median", {
+        
+        expect_true(equal_df(CollapseReplicates(PhyloExpressionSetExample[1:5,1:9],c(2,2,3),median,c("S1","S2","S3")), TestCollapsedDF5))
+        
+})
+
+test_that("CollapseReplicates() works with variable nrep = c(2,2,3) and FUN = max", {
+        
+        expect_true(equal_df(CollapseReplicates(PhyloExpressionSetExample[1:5,1:9],c(2,2,3),max,c("S1","S2","S3")), TestCollapsedDF6))
         
 })
 
@@ -55,6 +86,12 @@ test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to
 })
 
 
-
-
+TestCollapsedDF_stage.names <- TestCollapsedDF1
+colnames(TestCollapsedDF_stage.names)[3:5] <- paste0("X",1:3)
+        
+test_that("default stage.names work properly when stage.names = NULL",{
+        
+        expect_true(equal_df(CollapseReplicates(PhyloExpressionSetExample[1:5,1:8],2,mean), TestCollapsedDF_stage.names))
+        
+})
 
