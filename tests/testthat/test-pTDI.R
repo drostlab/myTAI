b3fa16e0ff1f50c9f7a4655897224ef10fe1699d
HajkD <hajkdrost@googlemail.com>
2015-05-26 15:58:48 +0200

    update unit tests for pTAI() and pTDI()

--- tests/testthat/test-pTDI.R
+++ tests/testthat/test-pTDI.R
@@ -12,8 +12,9 @@ equal_df <- function(df1,df2){
 
 test_that("pTDI computes correct partial TDI contribution values...",{
         
-        expect_true(equal_df(pTAI(DivergenceExpressionSetExample),apply(pStrata(DivergenceExpressionSetExample),2,cumsum)))
+        expect_true(equal_df(pTDI(DivergenceExpressionSetExample),apply(pStrata(DivergenceExpressionSetExample),2,cumsum)))
         
+        expect_equal(round(as.vector(pTDI(DivergenceExpressionSetExample)[1, ]),7),c(0.2174378,0.2207644,0.2309211,0.2214881,0.2195601,0.2047938,0.1704023))
         
 })
 
