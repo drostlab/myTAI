0decee0e5036aa3cdbd8dd994faaf34b5ad0152d
HajkD <hajkdrost@googlemail.com>
2015-05-27 14:54:04 +0200

    adding unit tests for pTAI() and pTDI()

--- tests/testthat/test-pTAI.R
+++ tests/testthat/test-pTAI.R
@@ -16,6 +16,8 @@ test_that("pTAI computes correct partial TAI contribution values...",{
         
         expect_equal(round(as.vector(pTAI(PhyloExpressionSetExample)[1, ]),7),c(0.3929533,0.3935308, 0.4142106, 0.4115399, 0.4216806, 0.4178302, 0.3883815))
         
+        expect_equal(as.vector(TAI(PhyloExpressionSetExample)), as.vector(pTAI(PhyloExpressionSetExample)[12, ]))
+        
 })
 
 
--- tests/testthat/test-pTDI.R
+++ tests/testthat/test-pTDI.R
@@ -16,6 +16,8 @@ test_that("pTDI computes correct partial TDI contribution values...",{
         
         expect_equal(round(as.vector(pTDI(DivergenceExpressionSetExample)[1, ]),7),c(0.2174378,0.2207644,0.2309211,0.2214881,0.2195601,0.2047938,0.1704023))
         
+        expect_equal(as.vector(TDI(DivergenceExpressionSetExample)), as.vector(pTDI(DivergenceExpressionSetExample)[10, ]))
+        
 })
 
 
