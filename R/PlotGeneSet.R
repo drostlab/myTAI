99f465c3c9b6cc69748ce85dbb077be5fd926371
HajkD <hajkdrost@googlemail.com>
2015-06-01 18:38:58 +0200

    update documentation of PlotGeneSet()

--- R/PlotGeneSet.R
+++ R/PlotGeneSet.R
@@ -2,18 +2,31 @@
 #' @description
 #' This function simply visualizes the gene expression profiles of
 #' a defined subset of genes stored in the input \code{ExpressionSet}.
-#' @param ExpressionSet
-#' @param gene.set
-#' @param get.subset
-#' @param colors
+#' @param ExpressionSet a standard PhyloExpressionSet or DivergenceExpressionSet object.
+#' @param gene.set a character vector storing the gene ids for which gene expression profiles shall be visualized. 
+#' @param get.subset a logical value indicating whether or not an \code{ExpressionSet} subset of the selected \code{gene.set} should be retuned. 
+#' @param colors colors for gene expression profiles. Default: \code{colors = NULL}, hence default colours are used.
 #' @param ... additional parameters passed to \code{\link{matplot}}.
 #' @author Hajk-Georg Drost
 #' @examples
-#' data(PhyloExpressioNSetExample)
+#' data(PhyloExpressionSetExample)
 #' 
+#' # png("test_png.png",700,400)
+#' PlotGeneSet(ExpressionSet = PhyloExpressionSetExample, 
+#'             gene.set      = PhyloExpressionSetExample[1:5, 2], 
+#'             type          = "l", 
+#'             lty           = 1, 
+#'             lwd           = 4,
+#'             xlab          = "Ontogeny",
+#'             ylab          = "Expression Level")
+#' 
+#' # dev.off()
 #' @export
 
-PlotGeneSet <- function(ExpressionSet, gene.set, get.subset = FALSE, colors = NULL, ... ){
+PlotGeneSet <- function(ExpressionSet, 
+                        gene.set, 
+                        get.subset = FALSE, 
+                        colors     = NULL, ... ){
         
         is.ExpressionSet(ExpressionSet)
         
@@ -31,7 +44,11 @@ PlotGeneSet <- function(ExpressionSet, gene.set, get.subset = FALSE, colors = NU
         if (is.null(colors)){
                 
                 par(mar=c(5.1, 4.1, 4.1, 8.1), xpd = TRUE)
-                matplot(t(GeneSubSet[ , 3:ncols]), col = re.colors(length(GeneSubSet.indixes)),...)
+                matplot(t(GeneSubSet[ , 3:ncols]), 
+                        col  = re.colors(length(GeneSubSet.indixes)),
+                        xaxt = "n", ...)
+                axis(1,at = seq(1,(ncols-2),1), labels = names(ExpressionSet)[3:ncols])
+                
                 legend("topright", 
                        inset  = c(-0.2,0), 
                        legend = GeneSubSet[ , 2], 
@@ -48,7 +65,9 @@ PlotGeneSet <- function(ExpressionSet, gene.set, get.subset = FALSE, colors = NU
                 
                 par(mar=c(5.1, 4.1, 4.1, 8.1), xpd = TRUE)
                 matplot(t(GeneSubSet[ , 3:ncols]), 
-                        col = colors[1:length(GeneSubSet.indixes)], ...)
+                        col  = colors[1:length(GeneSubSet.indixes)], 
+                        xaxt = "n", ...)
+                axis(1,at = seq(1,(ncols-2),1), labels = names(ExpressionSet)[3:ncols])
                 
                 legend("topright", 
                        inset  = c(-0.2,0), 
