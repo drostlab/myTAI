1f4e3c32621db393cadf1ca799be3e7f7725b6da
HajkD <hajkdrost@googlemail.com>
2015-06-13 14:35:17 +0200

    make geom.mean() external function and add documentation

--- R/base.R
+++ R/base.R
@@ -191,18 +191,28 @@ std.error <- function(x)
         }
 }
 
+#' @title Geometric Mean
+#' @description 
+#' This function computes the geometric mean of a numeric input vector \code{x}.
+#' @param x a numeric vector for which geometric mean computations shall be performed.
+#' @author Hajk-Georg Drost
+#' @examples 
+#' x <- 1:10
+#' 
+#' geom.mean(x)
+#' 
+#' @export
+
 geom.mean <- function(x)
 {
-        
         if(is.numeric(x)){
                 return(cpp_geom_mean(as.vector(x)))
-        }
-        
-        else{
+        } else{
                 stop("Please enter a numeric vector.")
         }
-        
 }
+
+
 
 harmonic.mean <- function(x)
 {
