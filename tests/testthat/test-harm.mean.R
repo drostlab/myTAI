cd7f38b67441b7f06809e95760bd7f11f6f14d33
HajkD <hajkdrost@googlemail.com>
2015-06-15 15:42:34 +0200

    adding unit test for harm.mean()

--- tests/testthat/test-harm.mean.R
+++ tests/testthat/test-harm.mean.R
@@ -14,6 +14,7 @@ test_harmonic_mean <- function(x)
 
 test_that("harm.mean() computes correct values",{
         
+        expect_equal(harm.mean(c(1,2,4)),12/7)
         expect_equal(harm.mean(1:10),test_harmonic_mean(1:10))
         expect_equal(harm.mean(1:100),test_harmonic_mean(1:100))
         expect_equal(harm.mean(1:1000),test_harmonic_mean(1:1000))
