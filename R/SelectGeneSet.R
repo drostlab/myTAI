22b03a71308033368834178b4d903fa756708a12
HajkD <hajkdrost@googlemail.com>
2015-07-20 12:19:31 +0100

    SelectGeneSet() now has a new argument 'use.only.map' specifying whether or not instead of using a standard ExpressionSet a Phylostratigraphic Map or Divergene Map is passed to the function.

--- R/SelectGeneSet.R
+++ R/SelectGeneSet.R
@@ -2,7 +2,8 @@
 #' @description
 #' Select a subset of genes stored in the input \code{ExpressionSet}.
 #' @param ExpressionSet a standard PhyloExpressionSet or DivergenceExpressionSet object.
-#' @param gene.set a character vector storing the gene ids for which gene expression profiles shall be visualized. 
+#' @param gene.set a character vector storing the gene ids for which gene expression profiles shall be visualized.
+#' @param use.only.map a logical value indicating whether instead of a standard \code{ExpressionSet} only a \code{Phylostratigraphic Map} or \code{Divergene Map} is passed to the function. 
 #' @author Hajk-Georg Drost
 #' @details
 #' 
@@ -10,7 +11,6 @@
 #' 
 #' This function is useful for studying the evolutionary \emph{properties} of a subset of genes
 #' stored in the \code{ExpressionSet}. 
-#' 
 #' 
 #' @examples
 #' data(PhyloExpressionSetExample)
@@ -20,14 +20,23 @@
 #' SelectGeneSet(ExpressionSet = PhyloExpressionSetExample,
 #'             gene.set      = PhyloExpressionSetExample[1:5, 2])
 #'             
-#' @seealso \code{\link{PlotGeneSet}}, \code{\link{PlotEnrichment}}, \code{\link{DiffGenes}}             
+#'             
+#' # get a gene subset using only a phylostratihraphic map
+#' ExamplePSMap <- PhyloExpressionSetExample[ , 1:2]
+#' 
+#' SelectGeneSet(ExpressionSet = ExamplePSMap,
+#'               gene.set      = PhyloExpressionSetExample[1:5, 2],
+#'               use.only.map  = TRUE)          
+#'             
+#' @seealso \code{\link{PlotGeneSet}}, \code{\link{PlotEnrichment}}, \code{\link{DiffGenes}}  
 #' @export
 
-SelectGeneSet <- function(ExpressionSet, gene.set) {
+SelectGeneSet <- function(ExpressionSet, gene.set, use.only.map = FALSE) {
         
         return( PlotGeneSet(ExpressionSet = ExpressionSet,
                             gene.set      = gene.set,
-                            get.subset    = TRUE) )
+                            get.subset    = TRUE,
+                            use.only.map  = use.only.map) )
         
         
 }
