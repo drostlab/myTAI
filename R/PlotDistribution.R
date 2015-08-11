fbec06ed763d883dad03430fbf4b42849c1a0a7d
HajkD <hajkdrost@googlemail.com>
2015-08-11 19:17:15 +0100

    - `PlotDistribution()` now uses ggplot2 to visualize the PS or DS distribution and is also based on the new function `PlotSelectedAgeDistr()`; furthermore it loses arguments `plotText` and `...` and gains a new argument `legendName`

--- R/PlotDistribution.R
+++ R/PlotDistribution.R
@@ -2,11 +2,10 @@
 #' @description This function plots the frequency distribution of genes within the 
 #' corresponding \emph{phylostratigraphic map} or \emph{divergence map} and can be used to fastly visualize the PS or DS distribution of a given phylostratum vector or divergence-stratum vector.
 #' @param PhyloExpressionSet a standard PhyloExpressionSet or DivergenceExpressionSet object.
-#' @param plotText a boolean value specifying whether the total number of genes 
-#' belonging to a corresponding phylostratum or divergence-stratum class
-#' shall be plotted above each bar of the barplot.
+#' @param legendName a character string specifying whether "PS" or "DS" are visualized.
 #' @param as.ratio a boolean value specifying whether the relative frequencies
 #' instead of absolute frequencies shall be plotted.
+#' @param use.only.map logical value indicating whether or not a Phylostratigraphic Map or Divergence Map should be passed to the \code{ExpressionSet} argument instead of a standard \code{ExpressionSet} object.
 #' @param \dots default plot parameters.
 #' @details 
 #' The frequency distribution of all genes or a subset of genes might be of interest for subsequent analyses.
@@ -28,60 +27,28 @@
 #' data(PhyloExpressionSetExample)
 #'
 #' # plot the phylostratum distribution of a PhyloExpressionSet
-#' PlotDistribution(PhyloExpressionSetExample, plotText = TRUE)
+#' PlotDistribution(PhyloExpressionSetExample)
 #'
 #' # plot the relative frequency distribution of a PhyloExpressionSet
-#' PlotDistribution(PhyloExpressionSetExample, plotText = TRUE, as.ratio = TRUE)
+#' PlotDistribution(PhyloExpressionSetExample, as.ratio = TRUE)
 #'
 #'
 #' # a example for visualizing the PS distribution for a subset of genes
-#' PlotDistribution(PhyloExpressionSetExample[sample(20000,5000) , ],
-#'                  plotText = TRUE, as.ratio = TRUE)
+#' PlotDistribution(PhyloExpressionSetExample[sample(20000,5000) , ], as.ratio = TRUE)
 #'
+#' @seealso \code{\link{PlotSelectedAgeDistr}}
 #' @export
 
-PlotDistribution <- function(PhyloExpressionSet, plotText = TRUE, as.ratio = FALSE, ...)
+PlotDistribution <- function(PhyloExpressionSet, legendName = "PS", as.ratio = FALSE, use.only.map = FALSE)
 {
         
-        is.ExpressionSet(PhyloExpressionSet)
         
-        AgeVector <- PhyloExpressionSet[ , 1]
-        minPSVal <- min(AgeVector)
-        maxPSVal <- max(AgeVector)
+        PlotSelectedAgeDistr(ExpressionSet = PhyloExpressionSet,
+                             gene.set      = PhyloExpressionSet[ , 2],
+                             legendName    = legendName,
+                             as.ratio      = as.ratio,
+                             use.only.map  = use.only.map)
         
-        nPS <- length(minPSVal:maxPSVal)
         
-        # define arguments for different graphics functions
-        barplot.args <- c("xlab","cex.lab","cex.axis","horiz","main","density","add","cex.names")
-        text.args <- c("cex")
-        dots <- list(...)
-        ellipsis.names <- names(dots)
-        
-        if(as.ratio == FALSE){
-                PStable <- table(factor(AgeVector,levels = 1:nPS))
-                yLim <- ceiling((max(PStable) + (max(PStable)/2)))
-                BarPlot <- do.call(graphics::barplot,c(list(PStable,ylab = "# Genes",ylim = c(0,yLim),border = "white"),
-                                                       dots[!is.element(names(dots),c(text.args))]))
-                
-                if(plotText == TRUE){
-                        do.call(graphics::text,c(list(x = BarPlot,y = (PStable + (PStable/3)),labels = ifelse(PStable > 0,PStable,"")),
-                                                 dots[!is.element(names(dots),c(barplot.args))]))
-                }
-        }
-        
-        if(as.ratio == TRUE){
-                
-                PStable <- table(factor(AgeVector,levels = 1:nPS))
-                relFreq <- as.numeric(format(PStable / sum(PStable),digits = 1))
-                names(relFreq) <- names(PStable)
-                yLim <- c(0,1)
-                BarPlot <- do.call(graphics::barplot,c(list(relFreq,ylab = "rel. frequency of genes",ylim = yLim,border = "white"),
-                                                       dots[!is.element(names(dots),c(text.args))]))
-                
-                if(plotText == TRUE){
-                        do.call(graphics::text,c(list(x = BarPlot,y = (relFreq + .1),labels = ifelse(relFreq > 0,relFreq,"")),
-                                                 dots[!is.element(names(dots),c(barplot.args))]))
-                }
-        }
 }
 
