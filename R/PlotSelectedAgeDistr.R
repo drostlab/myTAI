30e41aa5da41a2ccd74b3b0dcce77c6696b60f00
HajkD <hajkdrost@googlemail.com>
2015-08-11 19:19:38 +0100

    fix correct ordering of PS/DS when visualizing the PS or DS distribution

--- R/PlotSelectedAgeDistr.R
+++ R/PlotSelectedAgeDistr.R
@@ -3,7 +3,7 @@
 #' as histogram.
 #' @param ExpressionSet a standard PhyloExpressionSet or DivergenceExpressionSet object.
 #' @param gene.set a character vector storing the gene ids for which gene expression profiles shall be visualized.
-#' @param legendName a character string specifying whether "PS" or "DS" are used to compute relative expression profiles.
+#' @param legendName a character string specifying whether "PS" or "DS" are are visualized.
 #' @param as.ratio logical value indicating whether or not relative frequencies shall be visualized.
 #' @param use.only.map logical value indicating whether or not a Phylostratigraphic Map or Divergence Map should be passed to the \code{ExpressionSet} argument instead of a standard \code{ExpressionSet} object.
 #' @param col colour of the bars.
@@ -59,13 +59,13 @@ PlotSelectedAgeDistr <- function(ExpressionSet, gene.set,
         if (as.ratio){
                 ylab.name <- "Rel. Number Of  Genes\n"
                 AgeDistr <- table(factor(GeneSubSet[ , 1], levels = 1:nPS))
-                AgeDistr <- AgeDistr / sum(AgeDistr)
+                AgeDistr <- format(AgeDistr / sum(AgeDistr), digits = 2)
         }
                 
         GeneSubSet.LongFormat <- data.frame(AgeCategory = paste0(legendName,names(AgeDistr)), GeneCount = as.numeric(AgeDistr))
-        
-        GeneSubSet.LongFormat[ , 1] <- factor(GeneSubSet.LongFormat[ , 1], ordered = TRUE)
-        plot.res <- ggplot2::ggplot(GeneSubSet.LongFormat, ggplot2::aes(x = AgeCategory, y = GeneCount)) + ggplot2::geom_bar(stat="identity", fill = col) + ggplot2::labs(x = paste0("\n",legendName), y = ylab.name)  + ggplot2::geom_text(aes( label = GeneCount, y =  GeneCount), stat= "identity", vjust = -.3) + ggplot2::theme_minimal()
+        # get the correct order of PS or DS by specifying levels = paste0(legendName,names(AgeDistr))
+        GeneSubSet.LongFormat[ , 1] <- factor(GeneSubSet.LongFormat[ , 1], levels = paste0(legendName,names(AgeDistr)))
+        plot.res <- ggplot2::ggplot(GeneSubSet.LongFormat, ggplot2::aes(x = AgeCategory, y = GeneCount), order = FALSE) + ggplot2::geom_bar(stat="identity", fill = col) + ggplot2::labs(x = paste0("\n",legendName), y = ylab.name)  + ggplot2::geom_text(aes( label = GeneCount, y =  GeneCount), stat= "identity", vjust = -.3) + ggplot2::theme_minimal()
         
         return (plot.res)
 }
