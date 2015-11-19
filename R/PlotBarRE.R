a6e6f68324e3502669ad351450e22bf8affe443e
HajkD <hajkdrost@googlemail.com>
2015-11-19 17:28:40 +0000

    update documentation of PlotBarRE()

--- R/PlotBarRE.R
+++ R/PlotBarRE.R
@@ -4,23 +4,23 @@
 #' the differences of mean relative expression of these two (or more) corresponding phylostratum (divergence stratum) classes.
 #' As test-statistic, the function performs a nonparametric \code{\link{kruskal.test}}
 #'  based on relative expression values stored within each defined phylostratum class.
-#'  @param ExpressionSet a standard PhyloExpressionSet or DivergenceExpressionSet object.
-#'  @param Groups a list containing the phylostrata or divergence-strata that correspond to the same phylostratum class or divergence class.
+#' @param ExpressionSet a standard PhyloExpressionSet or DivergenceExpressionSet object.
+#' @param Groups a list containing the phylostrata or divergence-strata that correspond to the same phylostratum class or divergence class.
 #'  For ex. evolutionary old phylostrata: PS1-3 (Class 1) and evolutionary young phylostrata: PS4-12 (Class 2). 
 #'  In this case, the \code{Groups} list could be assigned as, \code{Groups} = list(c(1:3), c(4:12)). 
 #'  It is also possible to define more than 2 groups of evolutionary ages.
 #'  For ex. \code{Groups} = list(c(1:3),c(4:8),c(9:12)) would perform a \code{\link{kruskal.test}} to determine the statistical significance
 #'  of the evolutionary classes PS1-3, PS4-6, and PS9-12 based on their corresponding mean relative expression levels. 
-#'  @param wLength a numeric value defining the whiskers length above the bars. In case there are numerous different phylostratum classes
+#' @param wLength a numeric value defining the whiskers length above the bars. In case there are numerous different phylostratum classes
 #'  a smaller \code{wLength} parameter should be used for better visualizations.
-#'  @param ratio a boolean value specifying whether the bars in the barplot represent the 
+#' @param ratio a boolean value specifying whether the bars in the barplot represent the 
 #'  mean relative expression level of phylostrata belonging to the same phylostratum class.
 #'  In case \code{ratio} = TRUE, the ratio of the mean relative expression level of 
 #'  the two phylostrata classes is plotted as lines within the barplot. This parameter can only be used for 2 class comparisons.
-#'  @param p.adjust.method correction method to adjust p-values for multiple comparisons (see \code{\link{p.adjust}} for possible methods).
+#' @param p.adjust.method correction method to adjust p-values for multiple comparisons (see \code{\link{p.adjust}} for possible methods).
 #'   E.g., \code{p.adjust.method = "BH"} (Benjamini & Hochberg (1995)) or \code{p.adjust.method = "bonferroni"} (Bonferroni correction).
-#'  @param \dots default graphics parameters. 
-#'  @return A barplot comparing Phylostratum-Classes by its mean relative expression levels.
+#' @param \dots default graphics parameters. 
+#' @return A barplot comparing Phylostratum-Classes by its mean relative expression levels.
 #' Significant stages are marked by '*' referring to statistically significant differences:
 #'        
 #' (1) '*'   = P-Value <= 0.05 
