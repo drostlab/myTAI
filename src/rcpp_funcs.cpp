17314f1637201f38262e7b6e2c0a9d23d113b74a
HajkD <hajkdrost@googlemail.com>
2016-07-16 15:07:05 +0100

    update C++ export

--- src/rcpp_funcs.cpp
+++ src/rcpp_funcs.cpp
@@ -36,7 +36,7 @@ NumericVector permut(NumericVector a)
 }
 
 
-// @export
+//' @export
 // [[Rcpp::export]]
  NumericVector cpp_TAI(NumericMatrix ExpressionSet, NumericVector Phylostratum)
  {
@@ -58,7 +58,7 @@ NumericVector permut(NumericVector a)
 
   }
 
-// @export
+//' @export
 // [[Rcpp::export]]
 NumericMatrix cpp_bootMatrix(NumericMatrix ExpressionMatrix, NumericVector AgeVector, int permutations)
 {
@@ -107,7 +107,7 @@ NumericMatrix cpp_bootMatrix(NumericMatrix ExpressionMatrix, NumericVector AgeVe
   return bootM;
 }
 
-// @export
+//' @export
 // [[Rcpp::export]]
   NumericMatrix cpp_pMatrix(NumericMatrix ExpressionSet,NumericVector AgeVector)
   {
@@ -136,7 +136,7 @@ NumericMatrix cpp_bootMatrix(NumericMatrix ExpressionMatrix, NumericVector AgeVe
   }
 
 
-// @export
+//' @export
 // [[Rcpp::export]]
 double cpp_std_error(NumericVector x)
 {
@@ -145,7 +145,7 @@ double cpp_std_error(NumericVector x)
   
 }
   
-// @export 
+//' @export 
 // [[Rcpp::export]] 
 double cpp_geom_mean(NumericVector x)
 {
@@ -155,7 +155,7 @@ double cpp_geom_mean(NumericVector x)
 } 
 
 
-// @export
+//' @export
 // [[Rcpp::export]] 
 double cpp_harmonic_mean(NumericVector x)
 {
@@ -169,7 +169,7 @@ double cpp_harmonic_mean(NumericVector x)
 
 
 
-// @export
+//' @export
 // [[Rcpp::export]]
   NumericMatrix cpp_omitMatrix(NumericMatrix ExpressionSet, NumericVector AgeVector){
    
