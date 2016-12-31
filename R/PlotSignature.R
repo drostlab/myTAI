1c601c6752dacaf8909d6d39595092197c198379
HajkD <hajkdrost@googlemail.com>
2016-12-31 23:05:46 +0100

    add test statistics to PlotSignature()

--- R/PlotSignature.R
+++ R/PlotSignature.R
@@ -1,6 +1,5 @@
 #' @title Plot evolutionary signatures across transcriptomes  
-#' @description E
-#' 
+#' @description Main function to visualize transcriptome indices.
 #' @param ExpressionSet a standard PhyloExpressionSet or DivergenceExpressionSet object.
 #' @param TestStatistic a string defining the type of test statistics to be used to quantify the statistical significance the present phylotranscriptomics pattern.
 #' Possible values can be: \code{TestStatistic} = \code{"FlatLineTest"} : Statistical test for the deviation from a flat line.
@@ -11,13 +10,38 @@
 #' or experiments that correspond to each module. For example, 
 #' \code{module} = \code{list(early = 1:2, mid = 3:5, late = 6:7)} devides a dataset storing seven developmental stages into 3 modules.
 #' @param permutations a numeric value specifying the number of permutations to be performed for the \code{\link{FlatLineTest}}, \code{\link{EarlyConservationTest}} or \code{\link{ReductiveHourglassTest}}.
-#' @param xlab
-#' @param ylab
-#' @param main
-#' @param lwd
-#' @param alpha
-#' @param y.ticks
+#' @param lillie.test a boolean value specifying whether the Lilliefors Kolmogorov-Smirnov Test shall be performed.
+#' @param p.value a boolean value specifying whether the p-value of the test statistic shall be printed within the plot area.
+#' @param shaded.area a boolean value specifying whether a shaded area shall 
+#' be drawn for the developmental stages defined to be the presumptive phylotypic period.
+#' @param custom.perm.matrix a custom \code{\link{bootMatrix}} (permutation matrix) to perform the underlying test statistic visualized by \code{PlotPattern}. Default is \code{custom.perm.matrix = NULL}.
+#' @param xlab label of x-axis.
+#' @param ylab label of y-axis.
+#' @param main figure title.
+#' @param lwd line width.
+#' @param alpha transparency of the shaded area. Default is \code{alpha = 0.1}.
+#' @param y.ticks number of ticks on the y-axis. Default is \code{ticks = 10}.
 #' @author Hajk-Georg Drost
+#' @details 
+#' This function substitutes the functionality of the \code{\link{PlotPattern}} function
+#' and is based on ggplot2 insead of base R graphics.
+#' @examples 
+#' data(PhyloExpressionSetExample)
+#' 
+#' # plot TAI pattern and perform flat line test
+#' PlotSignature(PhyloExpressionSetExample, 
+#'               measure       = "TAI", 
+#'               permutations  = 1000,
+#'               TestStatistic = "FlatLineTest",
+#'               ylab = "Transcriptome Age Index")
+#'               
+#' # plot TAI pattern and perform reductive hourglass test
+#' PlotSignature(PhyloExpressionSetExample, 
+#'               measure       = "TAI", 
+#'               permutations  = 1000,
+#'               TestStatistic = "ReductiveHourglassTest",
+#'               modules       = list(early = 1:2, mid = 3:5, late = 6:7),
+#'               ylab = "Transcriptome Age Index")
 #' @export
 
 PlotSignature <-
@@ -26,6 +50,10 @@ PlotSignature <-
                  TestStatistic = "FlatLineTest",
                  modules = NULL,
                  permutations = 1000,
+                 lillie.test  = FALSE,
+                 p.value = TRUE,
+                 shaded.area  = TRUE,
+                 custom.perm.matrix = NULL,
                  xlab = "Ontogeny",
                  ylab = "Transcriptome Index",
                  main = "",
@@ -35,8 +63,17 @@ PlotSignature <-
                 
         
         if (!is.element(measure, c("TAI", "TDI", "TPI")))
-                stop("Measure '",measure,"' is not available for this function. Please specify a measure supporting by this function.", call.= FALSE)
+                stop(
+                        "Measure '",
+                        measure,
+                        "' is not available for this function. Please specify a measure supporting by this function.",
+                        call. = FALSE
+                )
         
+        cat("Plot signature: '",measure, "' and test statistic: '",TestStatistic,"'." )
+        cat("\n")
+                
+                
         # store transcriptome index in tibble
         if (measure == "TAI") {
                 TI <-
@@ -53,6 +90,113 @@ PlotSignature <-
         # generate bootMatrix
         bm <- tibble::as_tibble(bootMatrix(ExpressionSet = ExpressionSet, 
                                            permutations  = permutations))
+        
+        
+        if (TestStatistic == "FlatLineTest") {
+                if (is.null(custom.perm.matrix)) {
+                        resList <- FlatLineTest(ExpressionSet = ExpressionSet,
+                                                permutations  = permutations)
+                }
+                
+                else if (!is.null(custom.perm.matrix)) {
+                        resList <- FlatLineTest(ExpressionSet      = ExpressionSet,
+                                                custom.perm.matrix = custom.perm.matrix)
+                        
+                }
+        }
+        
+        if (TestStatistic == "ReductiveHourglassTest") {
+                if (lillie.test) {
+                        if (is.null(custom.perm.matrix)) {
+                                resList <- ReductiveHourglassTest(
+                                        ExpressionSet = ExpressionSet,
+                                        modules       = modules,
+                                        permutations  = permutations,
+                                        lillie.test   = TRUE
+                                )
+                        }
+                        
+                        else if (!is.null(custom.perm.matrix)) {
+                                resList <-
+                                        ReductiveHourglassTest(
+                                                ExpressionSet      = ExpressionSet,
+                                                modules            = modules,
+                                                lillie.test        = TRUE,
+                                                custom.perm.matrix = custom.perm.matrix
+                                        )
+                        }
+                }
+                
+                if (!lillie.test) {
+                        if (is.null(custom.perm.matrix)) {
+                                resList <- ReductiveHourglassTest(
+                                        ExpressionSet = ExpressionSet,
+                                        modules       = modules,
+                                        permutations  = permutations,
+                                        lillie.test   = FALSE
+                                )
+                        }
+                        
+                        else if (!is.null(custom.perm.matrix)) {
+                                resList <-
+                                        ReductiveHourglassTest(
+                                                ExpressionSet      = ExpressionSet,
+                                                modules            = modules,
+                                                lillie.test        = FALSE,
+                                                custom.perm.matrix = custom.perm.matrix
+                                        )
+                        }
+                }
+        }
+        
+        if (TestStatistic == "EarlyConservationTest") {
+                if (lillie.test) {
+                        if (is.null(custom.perm.matrix)) {
+                                resList <- EarlyConservationTest(
+                                        ExpressionSet = ExpressionSet,
+                                        modules       = modules,
+                                        permutations  = permutations,
+                                        lillie.test   = TRUE
+                                )
+                        }
+                        
+                        else if (!is.null(custom.perm.matrix)) {
+                                resList <-
+                                        EarlyConservationTest(
+                                                ExpressionSet      = ExpressionSet,
+                                                modules            = modules,
+                                                lillie.test        = TRUE,
+                                                custom.perm.matrix = custom.perm.matrix
+                                        )
+                        }
+                }
+                
+                if (!lillie.test) {
+                        if (is.null(custom.perm.matrix)) {
+                                resList <- EarlyConservationTest(
+                                        ExpressionSet = ExpressionSet,
+                                        modules       = modules,
+                                        permutations  = permutations,
+                                        lillie.test   = FALSE
+                                )
+                        }
+                        
+                        else if (!is.null(custom.perm.matrix)) {
+                                resList <-
+                                        EarlyConservationTest(
+                                                ExpressionSet      = ExpressionSet,
+                                                modules            = modules,
+                                                lillie.test        = FALSE,
+                                                custom.perm.matrix = custom.perm.matrix
+                                        )
+                        }
+                        
+                }
+        }
+        
+        # get p-value and standard deviation values from the test statistic
+        pval <- resList$p.value
+        pval <- format(pval,digits = 3)
         
         TI.ggplot <- ggplot2::ggplot(TI, ggplot2::aes(
                 x = factor(Stage, levels = unique(Stage)),
@@ -81,7 +225,74 @@ PlotSignature <-
                 ) +
                 ggplot2::scale_y_continuous(breaks = scales::pretty_breaks(n = y.ticks))
         
+        if ((TestStatistic == "FlatLineTest") && p.value){
+                TI.ggplot <-
+                        TI.ggplot + ggplot2::annotate(
+                                "text",
+                                x = 2,
+                                y = max(TI$TI) + (max(TI$TI) / 10),
+                                label = paste0("p_flt = ", pval),
+                                size = 6
+                        )
+                cat("Significance status of signature: ", ifelse(as.numeric(pval) <= 0.05, "significant.","not significant (= no evolutionary signature in the transcriptome)."))
+                cat("\n")
+        }
+                
+        if (TestStatistic == "ReductiveHourglassTest"){
+                
+                if (p.value) {
+                        TI.ggplot <-
+                                TI.ggplot + ggplot2::annotate(
+                                        "text",
+                                        x = 2,
+                                        y = max(TI$TI) + (max(TI$TI) / 30),
+                                        label = paste0("p_rht = ", pval),
+                                        size = 6
+                                )  
+                }
+                
+                if (shaded.area) {
+                        TI.ggplot <-
+                                TI.ggplot + ggplot2::geom_rect(data = TI,ggplot2::aes(
+                                        xmin = modules[[2]][1],
+                                        xmax = modules[[2]][length(modules[[2]])],
+                                        ymin = min(TI) - (min(TI) / 50),
+                                        ymax = Inf), fill = "#4d004b", alpha = alpha)  
+                }
+                
+                stage.names <- names(ExpressionSet)[3:ncol(ExpressionSet)]
+                cat("Modules: \n early = {",paste0(stage.names[modules[[1]]], " "),"}","\n","mid = {",paste0(stage.names[modules[[2]]], " "),"}","\n","late = {",paste0(stage.names[modules[[3]]], " "),"}")
+                cat("\n")
+                cat("Significance status of signature: ", ifelse(as.numeric(pval) <= 0.05, "significant.","not significant (= no evolutionary signature in the transcriptome)."))
+        }   
         
+        if (TestStatistic == "EarlyConservationTest"){
+                
+                if (p.value) {
+                        TI.ggplot <-
+                                TI.ggplot + ggplot2::annotate(
+                                        "text",
+                                        x = 2,
+                                        y = max(TI$TI) + (max(TI$TI) / 30),
+                                        label = paste0("p_ect = ", pval),
+                                        size = 6
+                                )  
+                }
+                
+                if (shaded.area) {
+                        TI.ggplot <-
+                                TI.ggplot + ggplot2::geom_rect(data = TI,ggplot2::aes(
+                                        xmin = modules[[2]][1],
+                                        xmax = modules[[2]][length(modules[[2]])],
+                                        ymin = min(TI) - (min(TI) / 50),
+                                        ymax = Inf), fill = "#009E73", alpha = alpha * 0.5)  
+                }
+                
+                stage.names <- names(ExpressionSet)[3:ncol(ExpressionSet)]
+                cat("Modules: \n early = {",paste0(stage.names[modules[[1]]], " "),"}","\n","mid = {",paste0(stage.names[modules[[2]]], " "),"}","\n","late = {",paste0(stage.names[modules[[3]]], " "),"}")
+                cat("\n")
+                cat("Significance status of signature: ", ifelse(as.numeric(pval) <= 0.05, "significant.","not significant (= no evolutionary signature in the transcriptome)."))
+        }
         return (TI.ggplot)
 }
 
