3e2b43e9442c7ca95d11a43ec1fd0b9fb88ea2b8
HajkD <hajkdrost@googlemail.com>
2017-01-08 15:28:05 +0100

    extend ggplot2 graphics and add xlab and ylab arguments

--- R/PlotCorrelation.R
+++ R/PlotCorrelation.R
@@ -11,19 +11,21 @@
 #' @param method a character string specifying the correlation method to cbe used, e.g. "pearson", "kendall", "spearman".  
 #' @param linearModel a boolean value specifying whether a linear model should be
 #' fitted to the data and furthermore, should be visualized in the corresponding plot.
+#' @param xlab label of x-axis.
+#' @param ylab label of y-axis.
 #' @return a jitter-correlation-plot of PS and DS correlation.
 #' @references  Quint M et al. (2012). \emph{A transcriptomic hourglass in plant embryogenesis}. Nature (490): 98-101.
 #' Drost HG et al. (2015) \emph{Evidence for Active Maintenance of Phylotranscriptomic Hourglass Patterns in Animal and Plant Embryogenesis}. Mol Biol Evol. 32 (5): 1221-1231 doi:10.1093/molbev/msv012.
 #' @author Hajk-Georg Drost
 #' @seealso \code{\link{cor}}
 #' @examples 
-#' 
-#'  # read standard phylotranscriptomics data
-#'  data(PhyloExpressionSetExample)
-#'  data(DivergenceExpressionSetExample)
+#' # read standard phylotranscriptomics data
+#' data(PhyloExpressionSetExample)
+#' data(DivergenceExpressionSetExample)
 #'  
 #' # plot the PS and DS correlation
-#' PlotCorrelation(PhyloExpressionSetExample, DivergenceExpressionSetExample, 
+#' PlotCorrelation(PhyloExpressionSetExample, 
+#'                 DivergenceExpressionSetExample, 
 #'                 method      = "pearson", 
 #'                 linearModel = TRUE)
 #' 
@@ -31,9 +33,11 @@
 #' 
 #' @export
 
-PlotCorrelation <- function(PhyloExpressionSet,DivergenceExpressionSet,
+PlotCorrelation <- function(PhyloExpressionSet, DivergenceExpressionSet,
                             method      = "pearson",
-                            linearModel = FALSE)
+                            linearModel = FALSE,
+                            xlab = "Phylostratum",
+                            ylab = "Divergencestratum")
 {
         
         is.ExpressionSet(PhyloExpressionSet)
@@ -44,40 +48,80 @@ PlotCorrelation <- function(PhyloExpressionSet,DivergenceExpressionSet,
         
         colnames(PhyloExpressionSet)[2] <- "GeneID"
         colnames(DivergenceExpressionSet)[2] <- "GeneID"
-        nPS <- length(names(table(PhyloExpressionSet[ , 1])))
-        nDS <- length(names(table(DivergenceExpressionSet[ , 1])))
         
         # convert ids to lower case
         PhyloExpressionSet[ , "GeneID"] <- tolower(PhyloExpressionSet[ , "GeneID"])
         DivergenceExpressionSet[ , "GeneID"] <- tolower(DivergenceExpressionSet[ , "GeneID"])
         
-        PS_DS.Subset <- merge(PhyloExpressionSet[ , 1:2], DivergenceExpressionSet[ , 1:2],by = "GeneID")
+        PS_DS.Subset <-
+                merge(PhyloExpressionSet[ , 1:2], DivergenceExpressionSet[ , 1:2], by = "GeneID")
         
-        CorrelationCoefficient <- stats::cor(PS_DS.Subset[ , 2],PS_DS.Subset[ , 3],method = method)
-        CorrCoeffasCharacter <- as.character(round(CorrelationCoefficient,3))
+        CorrelationCoefficient <-
+                stats::cor(PS_DS.Subset[, 2], PS_DS.Subset[, 3], method = method)
+        CorrCoeffasCharacter <-
+                as.character(round(CorrelationCoefficient, 3))
         
         nrows <- dim(PS_DS.Subset)[1]
         PS <- vector(mode = "numeric", length = nrows)
         DS <- vector(mode = "numeric", length = nrows)
         
-        PS <- jitter(PS_DS.Subset[ , 2],1.5)
-        DS <- jitter(PS_DS.Subset[ , 3],1.5)
+        PS <- jitter(PS_DS.Subset[ , 2], 1.5)
+        DS <- jitter(PS_DS.Subset[ , 3], 1.5)
         
         CorDF <- data.frame(PS = PS, DS = DS)
         
-        if (!linearModel){
-                
-                res.plot <- ggplot2::ggplot(CorDF, ggplot2::aes(x=PS, y=DS)) + ggplot2::ggtitle(paste0(method," = ",CorrCoeffasCharacter)) +
-                        ggplot2::geom_point(shape=1) +  ggplot2::theme_minimal()
+        if (!linearModel) {
+                res.plot <-
+                        ggplot2::ggplot(CorDF, ggplot2::aes(x = PS, y = DS)) + 
+                        ggplot2::ggtitle(paste0(method, " = ", CorrCoeffasCharacter)) +
+                        ggplot2::geom_point(shape = 1) +  
+                        ggplot2::theme_minimal() + 
+                        ggplot2::labs(x = xlab, y = ylab) +
+                        ggplot2::theme(
+                                title            = ggplot2::element_text(size = 18, face = "bold"),
+                                legend.title     = ggplot2::element_text(size = 25, face = "bold"),
+                                legend.text      = ggplot2::element_text(size = 18, face = "bold"),
+                                axis.title       = ggplot2::element_text(size = 25, face = "bold"),
+                                axis.text.y      = ggplot2::element_text(size = 18, face = "bold"),
+                                axis.text.x      = ggplot2::element_text(size = 18, face = "bold"),
+                                panel.background = ggplot2::element_blank(),
+                                strip.text.x     = ggplot2::element_text(
+                                        size           = 25,
+                                        colour         = "black",
+                                        face           = "bold"
+                                )
+                        ) +
+                        
+                        ggplot2::scale_x_continuous(breaks = as.numeric(names(table(PS_DS.Subset[ , 2])))) + 
+                        ggplot2::scale_y_continuous(breaks = as.numeric(names(table(PS_DS.Subset[ , 3]))))
                 
         }
         
-        if (linearModel){
-         
-                res.plot <- ggplot2::ggplot(CorDF, ggplot2::aes(x=PS, y=DS)) + ggplot2::ggtitle(paste0(method," = ",CorrCoeffasCharacter)) +
-                        ggplot2::geom_point(shape=1) + 
-                        ggplot2::geom_smooth(method="auto") + ggplot2::theme_minimal()       
-                
+        if (linearModel) {
+                res.plot <-
+                        ggplot2::ggplot(CorDF, ggplot2::aes(x = PS, y = DS)) + 
+                        ggplot2::ggtitle(paste0(method, " = ", CorrCoeffasCharacter)) +
+                        ggplot2::geom_point(shape = 1) +
+                        ggplot2::geom_smooth(method = "gam") + 
+                        ggplot2::theme_minimal() + 
+                        ggplot2::labs(x = xlab, y = ylab) +
+                        ggplot2::theme(
+                                title            = ggplot2::element_text(size = 18, face = "bold"),
+                                legend.title     = ggplot2::element_text(size = 25, face = "bold"),
+                                legend.text      = ggplot2::element_text(size = 18, face = "bold"),
+                                axis.title       = ggplot2::element_text(size = 25, face = "bold"),
+                                axis.text.y      = ggplot2::element_text(size = 18, face = "bold"),
+                                axis.text.x      = ggplot2::element_text(size = 18, face = "bold"),
+                                panel.background = ggplot2::element_blank(),
+                                strip.text.x     = ggplot2::element_text(
+                                        size           = 25,
+                                        colour         = "black",
+                                        face           = "bold"
+                                )
+                        ) +
+                        
+                        ggplot2::scale_x_continuous(breaks = as.numeric(names(table(PS_DS.Subset[ , 2])))) + 
+                        ggplot2::scale_y_continuous(breaks = as.numeric(names(table(PS_DS.Subset[ , 3]))))
         }
         
         return(res.plot)
