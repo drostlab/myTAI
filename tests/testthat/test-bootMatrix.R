a0abdaeb48e8a07a1b941d646f75e85a42f03441
HajkD <hajkdrost@googlemail.com>
2017-06-29 14:57:02 +0100

    update unit test

--- tests/testthat/test-bootMatrix.R
+++ tests/testthat/test-bootMatrix.R
@@ -5,25 +5,35 @@ data(PhyloExpressionSetExample)
 data(DivergenceExpressionSetExample)
 
 
-nonStandardExpressionSet <- PhyloExpressionSetExample[ , 2:9] 
+nonStandardExpressionSet <- PhyloExpressionSetExample[, 2:9]
 
-test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to bootMatrix()",{
-        expect_error(bootMatrix(nonStandardExpressionSet),"The present input object does not fulfill the ExpressionSet standard.")
-})
+test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to bootMatrix()",
+          {
+                  expect_error(
+                          bootMatrix(nonStandardExpressionSet)
+                  )
+          })
 
 
 test_that("# permutations matches nrow() of bootMatrix output...", {
-        expect_equal(nrow(bootMatrix(PhyloExpressionSetExample, permutations = 1)), 1)
-        expect_equal(nrow(bootMatrix(PhyloExpressionSetExample, permutations = 10)), 10)
-        expect_equal(nrow(bootMatrix(PhyloExpressionSetExample, permutations = 100)), 100)
-        expect_equal(nrow(bootMatrix(PhyloExpressionSetExample, permutations = 1000)), 1000)
+        expect_equal(nrow(
+                bootMatrix(PhyloExpressionSetExample, permutations = 1)
+        ), 1)
+        expect_equal(nrow(
+                bootMatrix(PhyloExpressionSetExample, permutations = 10)
+        ), 10)
+        expect_equal(nrow(
+                bootMatrix(PhyloExpressionSetExample, permutations = 100)
+        ), 100)
+        expect_equal(nrow(
+                bootMatrix(PhyloExpressionSetExample, permutations = 1000)
+        ), 1000)
         
 })
 
 
 test_that("bootMatrix() returns numeric values", {
-        expect_true(is.numeric(bootMatrix(PhyloExpressionSetExample, permutations = 10)))
+        expect_true(is.numeric(
+                bootMatrix(PhyloExpressionSetExample, permutations = 10)
+        ))
 })
-
-
-
