0d17286cb108f4670538cea8ccdcb2093f09665a
HajkD <hajkdrost@googlemail.com>
2017-06-29 14:57:06 +0100

    update unit test

--- tests/testthat/test-omitMatrix.R
+++ tests/testthat/test-omitMatrix.R
@@ -2,7 +2,7 @@ context("Test: omitMatrix() ")
 
 data(PhyloExpressionSetExample)
 
-# adapted from: https://github.com/hadley/dplyr/blob/master/tests/testthat/test-arrange.r 
+# adapted from: https://github.com/hadley/dplyr/blob/master/tests/testthat/test-arrange.r
 equal_df <- function(df1, df2) {
         rownames(df1) <- NULL
         rownames(df2) <- NULL
@@ -11,25 +11,27 @@ equal_df <- function(df1, df2) {
         isTRUE(all.equal(df1, df2))
 }
 
-nonStandardExpressionSet <- PhyloExpressionSetExample[ , 2:9] 
+nonStandardExpressionSet <- PhyloExpressionSetExample[, 2:9]
 
-test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to omitMatrix()",{
-        expect_error(omitMatrix(nonStandardExpressionSet),"The present input object does not fulfill the ExpressionSet standard.")
-})
+test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to omitMatrix()",
+          {
+                  expect_error(
+                          omitMatrix(nonStandardExpressionSet)
+                  )
+          })
 
 
-TestOmitMatrix <- omitMatrix(PhyloExpressionSetExample)[1:50 , ]
-TestOmitMatrix2 <- matrix(NA_real_,50,7)
+TestOmitMatrix <- omitMatrix(PhyloExpressionSetExample)[1:50 ,]
+TestOmitMatrix2 <- matrix(NA_real_, 50, 7)
 colnames(TestOmitMatrix2) <- names(PhyloExpressionSetExample)[3:9]
-rownames(TestOmitMatrix2) <- paste0("(-) ",PhyloExpressionSetExample[1:50,2])
+rownames(TestOmitMatrix2) <-
+        paste0("(-) ", PhyloExpressionSetExample[1:50, 2])
 
-for(i in 1:50) 
-        TestOmitMatrix2[i, ] <- TAI(PhyloExpressionSetExample[-c(i), ])
+for (i in 1:50)
+        TestOmitMatrix2[i,] <-
+        TAI(PhyloExpressionSetExample[-c(i),])
 
-test_that("omitMatrix() computes correct TAI/TDI values",{
-        
-        expect_true(equal_df(TestOmitMatrix,TestOmitMatrix2))
+test_that("omitMatrix() computes correct TAI/TDI values", {
+        expect_true(equal_df(TestOmitMatrix, TestOmitMatrix2))
         
 })
-
-
