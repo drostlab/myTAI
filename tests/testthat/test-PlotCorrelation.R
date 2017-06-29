66d565a6eb015e78a1a74c732acc206b7c89cab8
HajkD <hajkdrost@googlemail.com>
2017-06-29 14:56:14 +0100

    update unit test

--- tests/testthat/test-PlotCorrelation.R
+++ tests/testthat/test-PlotCorrelation.R
@@ -3,8 +3,13 @@ context("Test: PlotCorrelation() ")
 data(PhyloExpressionSetExample)
 data(DivergenceExpressionSetExample)
 
-nonStandardExpressionSet <- PhyloExpressionSetExample[ , 2:9] 
+nonStandardExpressionSet <- PhyloExpressionSetExample[, 2:9]
 
-test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to PlotCorrelation()",{
-        expect_error(PlotCorrelation(nonStandardExpressionSet,nonStandardExpressionSet),"The present input object does not fulfill the ExpressionSet standard.")
-})
+test_that(
+        "is.ExpressionSet() throughs error when no ExpressionSet is entered to PlotCorrelation()",
+        {
+                expect_error(
+                        PlotCorrelation(nonStandardExpressionSet, nonStandardExpressionSet)
+                )
+        }
+)
