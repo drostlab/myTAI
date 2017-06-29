9be3be95c406bf663998da0149f2387f50fc6f2a
HajkD <hajkdrost@googlemail.com>
2017-06-29 14:56:29 +0100

    update unit test

--- tests/testthat/test-PlotPattern.R
+++ tests/testthat/test-PlotPattern.R
@@ -3,8 +3,11 @@ context("Test: PlotPattern() ")
 data(PhyloExpressionSetExample)
 data(DivergenceExpressionSetExample)
 
-nonStandardExpressionSet <- PhyloExpressionSetExample[ , 2:9] 
+nonStandardExpressionSet <- PhyloExpressionSetExample[, 2:9]
 
-test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to PlotPattern()",{
-        expect_error(PlotPattern(nonStandardExpressionSet),"The present input object does not fulfill the ExpressionSet standard.")
-})
+test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to PlotPattern()",
+          {
+                  expect_error(
+                          PlotPattern(nonStandardExpressionSet)
+                  )
+          })
