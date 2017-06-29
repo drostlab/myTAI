ce22a64d03b29ec53f4e54e176d3304be89cbdcf
HajkD <hajkdrost@googlemail.com>
2017-06-29 14:56:56 +0100

    update unit test

--- tests/testthat/test-TDI.R
+++ tests/testthat/test-TDI.R
@@ -3,19 +3,24 @@ context("Test: TDI() ")
 data(DivergenceExpressionSetExample)
 
 
-nonStandardExpressionSet <- DivergenceExpressionSetExample[ , 2:9] 
+nonStandardExpressionSet <- DivergenceExpressionSetExample[, 2:9]
 
-test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to TDI()",{
-        expect_error(TDI(nonStandardExpressionSet),"The present input object does not fulfill the ExpressionSet standard.")
-})
+test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to TDI()",
+          {
+                  expect_error(
+                          TDI(nonStandardExpressionSet)
+                  )
+          })
 
 
-testTAIFunc <- function(ExpressionSet){
+testTAIFunc <- function(ExpressionSet) {
         ncols <- ncol(ExpressionSet)
-        TAIVals <- vector("numeric",ncols-2) 
+        TAIVals <- vector("numeric", ncols - 2)
         
-        for(i in 1:(ncols-2)){
-                TAIVals[i] <-  sum((ExpressionSet[ , 1] * ExpressionSet[ , i+2])/sum(ExpressionSet[ , i+2]))
+        for (i in 1:(ncols - 2)) {
+                TAIVals[i] <-
+                        sum((ExpressionSet[, 1] * ExpressionSet[, i + 2]) / sum(ExpressionSet[, i +
+                                                                                                      2]))
         }
         
         names(TAIVals) <- names(ExpressionSet)[3:ncols]
@@ -24,11 +29,9 @@ testTAIFunc <- function(ExpressionSet){
 }
 
 
-test_that("TDI() computes correct values ",{
-        expect_equal(TDI(DivergenceExpressionSetExample),testTAIFunc(DivergenceExpressionSetExample))
+test_that("TDI() computes correct values ", {
+        expect_equal(
+                TDI(DivergenceExpressionSetExample),
+                testTAIFunc(DivergenceExpressionSetExample)
+        )
 })
-
-
-
-
-
