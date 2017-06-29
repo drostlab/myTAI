d96034c53aebb1a6da59c6f073ac995a23565b9c
HajkD <hajkdrost@googlemail.com>
2017-06-29 14:56:12 +0100

    update unit test

--- tests/testthat/test-PlotContribution.R
+++ tests/testthat/test-PlotContribution.R
@@ -3,34 +3,32 @@ context("Test: PlotContribution() ")
 data(PhyloExpressionSetExample)
 data(DivergenceExpressionSetExample)
 
-nonStandardExpressionSet <- PhyloExpressionSetExample[ , 2:9] 
+nonStandardExpressionSet <- PhyloExpressionSetExample[, 2:9]
 
 
-test_that("PlotContribution() works properly with PhyloExpressionSet input...",{
-        
-        expect_silent(PlotContribution(PhyloExpressionSetExample, legendName = "PS"))
+test_that("PlotContribution() works properly with PhyloExpressionSet input...",
+          {
+                  expect_silent(PlotContribution(PhyloExpressionSetExample, legendName = "PS"))
+          })
+
+test_that("PlotContribution() works properly with DivergenceExpressionSet input...",
+          {
+                  expect_silent(PlotContribution(DivergenceExpressionSetExample, legendName = "DS"))
+          })
+
+test_that(
+        "is.ExpressionSet() throughs error when no ExpressionSet is entered to PlotContribution()",
+        {
+                expect_error(
+                        PlotContribution(nonStandardExpressionSet, legendName = "PS")
+                )
+        }
+)
+
+
+test_that("Error occurs when legendName is not specified...", {
+        expect_error(
+                PlotContribution(PhyloExpressionSetExample),
+                "Please specify whether your input ExpressionSet stores 'PS' or 'DS'."
+        )
 })
-
-test_that("PlotContribution() works properly with DivergenceExpressionSet input...",{
-        
-        expect_silent(PlotContribution(DivergenceExpressionSetExample, legendName = "DS"))
-})
-
-test_that("is.ExpressionSet() throughs error when no ExpressionSet is entered to PlotContribution()",{
-        expect_error(PlotContribution(nonStandardExpressionSet, legendName = "PS"),"The present input object does not fulfill the ExpressionSet standard.")
-})
-
-
-test_that("Error occurs when legendName is not specified...",{
-        
-        expect_error(PlotContribution(PhyloExpressionSetExample),"Please specify whether your input ExpressionSet stores 'PS' or 'DS'.")
-})
-
-
-
-
-
-
-
-
-
