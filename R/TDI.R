0883ef6f911dfc4d03f3502e75e9f548adc3e880
HajkD <hgd23@cam.ac.uk>
2018-03-23 16:17:01 +0000

    enable tibble input

--- R/TDI.R
+++ R/TDI.R
@@ -38,12 +38,12 @@ TDI <- function(DivergenceExpressionSet)
         is.ExpressionSet(DivergenceExpressionSet)
         
         nCols <- dim(DivergenceExpressionSet)[2]
-        ExpressionMatrix <- DivergenceExpressionSet[ , 3:nCols]
-        Divergencestratum <- DivergenceExpressionSet[ , 1]
-        TDIProfile <- vector(mode = "numeric",length = nCols-2)
+        ExpressionMatrix <- dplyr::select(DivergenceExpressionSet, 3:ncol(DivergenceExpressionSet))
+        Divergencestratum <- unlist(dplyr::select(DivergenceExpressionSet, 1))
+        TDIProfile <- vector(mode = "numeric", length = nCols - 2)
         
         
-        TDIProfile <- cpp_TAI(as.matrix(ExpressionMatrix),as.vector(Divergencestratum))
+        TDIProfile <- cpp_TAI(as.matrix(ExpressionMatrix), as.vector(Divergencestratum))
         names(TDIProfile) <- names(ExpressionMatrix)
         
         return(TDIProfile)
