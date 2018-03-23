3d169575aa845244ae65e88b642e5a931c8ac28b
HajkD <hgd23@cam.ac.uk>
2018-03-23 15:51:10 +0000

    test for tibble as input and return tibble

--- R/is.ExpressionSet.R
+++ R/is.ExpressionSet.R
@@ -12,38 +12,42 @@
 #' @export
 is.ExpressionSet <- function(ExpressionSet){
         
-        ExpressionSet <- as.data.frame(ExpressionSet)
         ncols <- dim(ExpressionSet)[2]
         
-        d.f_bool <- is.data.frame(ExpressionSet)
+        d.f_bool <- is.data.frame(ExpressionSet) | tibble::is.tibble(ExpressionSet)
         
         if (!d.f_bool)
-                stop("ExpressionSet is not a data.frame.")
+                stop("ExpressionSet is not a data.frame or tibble.", call. = FALSE)
         
-        age.vector_bool <- is.numeric(ExpressionSet[ , 1])
+        age.vector_bool <- is.numeric(unlist(dplyr::select(ExpressionSet, 1)))
         
         if (!age.vector_bool)
-                stop("The first column of the ExpressionSet needs to store numeric values.")
+                stop("The first column of the ExpressionSet needs to store numeric values.", call. = FALSE)
         
-        gene.vector_bool <- ifelse(is.factor(ExpressionSet[ , 2]),is.character(levels(ExpressionSet[ , 2])),is.character(ExpressionSet[ , 2]))
+        gene.vector_bool <-
+            ifelse(is.factor(unlist(dplyr::select(ExpressionSet, 2))),
+                   is.character(levels(unlist(
+                       dplyr::select(ExpressionSet, 2)
+                   ))),
+                   is.character(unlist(dplyr::select(ExpressionSet, 2))))
         
         if (!gene.vector_bool)
-                stop("The second column of the ExpressionSet needs to store character values (e.g. gene ids).")
+                stop("The second column of the ExpressionSet needs to store character values (e.g. gene ids).", call. = FALSE)
         
-        expression.matrix_bool <- all(sapply(as.matrix(ExpressionSet[ , 3:ncols]), is.numeric))
+        expression.matrix_bool <- all(apply(dplyr::select(ExpressionSet, 3:ncol(ExpressionSet)), 2 , is.numeric))
         if (!expression.matrix_bool)
-                stop("Columns 3 - ",ncol(ExpressionSet), " need to store numeric values (e.g. expression levels).")
+                stop("Columns 3 - ",ncol(ExpressionSet), " need to store numeric values (e.g. expression levels).", call. = FALSE)
         
         any.NA.values_bool <- !any(is.na(ExpressionSet))
         
         if (!any.NA.values_bool)
-                stop("The ExpressionSet cannot contain 'NA' values. Please use na.omit() to remove 'NA' values from the input ExpressionSet.")
+                stop("The ExpressionSet cannot contain 'NA' values. Please use na.omit() to remove 'NA' values from the input ExpressionSet.", call. = FALSE)
         
-        if(all(c(d.f_bool,age.vector_bool,gene.vector_bool,expression.matrix_bool,any.NA.values_bool))){
+        if (all(c(d.f_bool,age.vector_bool,gene.vector_bool,expression.matrix_bool,any.NA.values_bool))) {
                 return(TRUE)
         }
         
         else{
-                stop("The present input object does not fulfill the ExpressionSet standard.")
+                stop("The present input object does not fulfill the ExpressionSet standard.", call. = FALSE)
         }
 }
