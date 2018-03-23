18484186d24197ce1898ebce2486a1abbcb9811d
HajkD <hgd23@cam.ac.uk>
2018-03-23 16:16:57 +0000

    enable tibble input

--- R/TAI.R
+++ R/TAI.R
@@ -44,11 +44,11 @@ TAI <- function(PhyloExpressionSet)
         is.ExpressionSet(PhyloExpressionSet)
         
         nCols <- dim(PhyloExpressionSet)[2]
-        ExpressionMatrix <- PhyloExpressionSet[ , 3:nCols]
-        Phylostratum <- PhyloExpressionSet[ , 1]
-        TAIProfile <- vector(mode = "numeric",length = nCols-2)
+        ExpressionMatrix <- dplyr::select(PhyloExpressionSet, 3:ncol(PhyloExpressionSet))
+        Phylostratum <- unlist(dplyr::select(PhyloExpressionSet, 1))
+        TAIProfile <- vector(mode = "numeric", length = nCols - 2)
         
-        TAIProfile <- cpp_TAI(as.matrix(ExpressionMatrix),as.vector(Phylostratum))
+        TAIProfile <- cpp_TAI(as.matrix(ExpressionMatrix), as.vector(Phylostratum))
         names(TAIProfile) <- names(ExpressionMatrix)
         
         return(TAIProfile)
