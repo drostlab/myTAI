cf474fb455ead2db9ef283d99059d8ddfe494e2b
HajkD <hajkdrost@googlemail.com>
2022-03-20 22:49:12 +0100

    update TEI()

--- R/TEI.R
+++ R/TEI.R
@@ -1,7 +1,7 @@
 #' @title Compute the Transcriptome Evolutionary Index (TEI)
 #' @description
 #' This function computes the phylogenetically based transcriptome evolutionary
-#' index (TEI) similar to the \code{\link{TAI}} introduced by \code{Domazet-Loso & Tautz, 2010}.
+#' index (TEI) similar to Domazet-Loso & Tautz, 2010.
 #' @param ExpressionSet expression object with rownames as GeneID (dgCMatrix)
 #' or standard PhyloExpressionSet object.
 #' @param Phylostratum a named vector representing phylostratum per GeneID with
@@ -34,142 +34,111 @@
 #' Drost HG et al. (2015)
 #' Mol Biol Evol. 32 (5): 1221-1231 doi:10.1093/molbev/msv012
 #'
-#' @author Kristian K Ullrich
 #' @examples
 #'
 #' # reading a standard PhyloExpressionSet
 #' data(PhyloExpressionSetExample, package = "myTAI")
 #'
 #' # computing the TEI profile of a given PhyloExpressionSet object
-#' TEIs <- TEI(PhyloExpressionSetExample)
+#' TEI <- TEI(PhyloExpressionSetExample)
 #'
-#' @export
+#' @export TEI
 #' @author Kristian K Ullrich
 
 TEI <- function(ExpressionSet,
-                Phylostratum = NULL,
-                split = 100000,
-                showprogress = TRUE,
-                threads = 1) {
-  if (is(ExpressionSet, "Matrix")) {
-    common_ids <- sort(Reduce(intersect, list(
-      rownames(ExpressionSet),
-      names(Phylostratum)
-    )))
-    Phylostratum <-
-      Phylostratum[names(Phylostratum) %in% common_ids]
-    Phylostratum <- Phylostratum[order(names(Phylostratum))]
-    if (ncol(ExpressionSet) > split) {
-      split_start <- seq(from = 1,
-                         to = ncol(ExpressionSet),
-                         by = split)
-      if (ncol(ExpressionSet) %% split != 0) {
-        split_end <- c(seq(
-          from = split,
-          to = ncol(ExpressionSet),
-          by = split
-        ),
-        ncol(ExpressionSet))
-      } else{
-        split_end <- seq(from = split,
-                         to = ncol(ExpressionSet),
-                         by = split)
+                Phylostratum=NULL,
+                split=100000,
+                showprogress=TRUE,
+                threads=1){
+  if(is(ExpressionSet, "Matrix")){
+    common_ids<-sort(Reduce(intersect, list(rownames(ExpressionSet),
+                                            names(Phylostratum))))
+    Phylostratum<-Phylostratum[names(Phylostratum) %in% common_ids]
+    Phylostratum<-Phylostratum[order(names(Phylostratum))]
+    if(ncol(ExpressionSet)>split){
+      split_start<-seq(from=1, to=ncol(ExpressionSet), by=split)
+      if(ncol(ExpressionSet)%%split!=0){
+        split_end<-c(seq(from=split, to=ncol(ExpressionSet),
+                         by=split), ncol(ExpressionSet))
+      }else{
+        split_end<-seq(from=split, to=ncol(ExpressionSet), by=split)
       }
-      if (rev(split_end - split_start)[1] == 0) {
-        split_start <- split_start[-length(split_start)]
-        split_end[length(split_end) - 1] <-
-          rev(split_end)[1]
-        split_end <- split_end[-length(split_end)]
+      if(rev(split_end-split_start)[1]==0){
+        split_start<-split_start[-length(split_start)]
+        split_end[length(split_end)-1] <- rev(split_end)[1]
+        split_end<-split_end[-length(split_end)]
       }
-      if (showprogress) {
-        pb <- txtProgressBar(min = 1,
-                             max = length(split_start),
-                             style = 3)
+      if(showprogress){
+        pb<-utils::txtProgressBar(min=1, max=length(split_start), style=3)
       }
-      OUT <- NULL
-      es <- NULL
-      for (i in seq_along(split_start)) {
-        es <- ExpressionSet[, split_start[i]:split_end[i]]
-        es <- es[rownames(es) %in% common_ids, , drop = FALSE]
-        es <- es[order(rownames(es)), ]
-        sumx_teisum_tei <-
-          rcpp_tei_parallel(es, Phylostratum, threads)
-        tei <- unlist(sumx_teisum_tei["tei"])
-        names(tei) <- colnames(es)
-        if (showprogress) {
-          setTxtProgressBar(pb, i)
-        }
-        es <- NULL
-        OUT <- c(OUT, tei)
-      }
-      tei <- OUT
-    } else{
-      ExpressionSet <- ExpressionSet[rownames(ExpressionSet) %in%
-                                       common_ids, , drop = FALSE]
-      ExpressionSet <-
-        ExpressionSet[order(rownames(ExpressionSet)), ]
-      sumx_teisum_tei <-
-        rcpp_tei_parallel(ExpressionSet, Phylostratum)
-      tei <- unlist(sumx_teisum_tei["tei"])
-      names(tei) <- colnames(ExpressionSet)
-    }
-  }
-  if (is(ExpressionSet, "data.frame") |
-      is(ExpressionSet, "tibble")) {
-    if (is.ExpressionSet(ExpressionSet)) {
-      Phylostratum <- setNames(ExpressionSet$Phylostratum,
-                               ExpressionSet$GeneID)
-      ExpressionSet <-
-        as(data.matrix(ExpressionSet[, 3:ncol(ExpressionSet)]), "sparseMatrix")
-      rownames(ExpressionSet) <- names(Phylostratum)
-    }
-    if (ncol(ExpressionSet) > split) {
-      split_start <- seq(from = 1,
-                         to = ncol(ExpressionSet),
-                         by = split)
-      if (ncol(ExpressionSet) %% split != 0) {
-        split_end <- c(seq(
-          from = split,
-          to = ncol(ExpressionSet),
-          by = split
-        ),
-        ncol(ExpressionSet))
-      } else{
-        split_end <- seq(from = split,
-                         to = ncol(ExpressionSet),
-                         by = split)
-      }
-      if (rev(split_end - split_start)[1] == 0) {
-        split_start <- split_start[-length(split_start)]
-        split_end[length(split_end) - 1] <-
-          rev(split_end)[1]
-        split_end <- split_end[-length(split_end)]
-      }
-      if (showprogress) {
-        pb <- utils::txtProgressBar(min = 1,
-                             max = length(split_start),
-                             style = 3)
-      }
-      OUT <- NULL
-      es <- NULL
-      for (i in seq_along(split_start)) {
-        es <- ExpressionSet[, split_start[i]:split_end[i]]
-        sumx_teisum_tei <-
-          rcpp_tei_parallel(es, Phylostratum, threads)
-        tei <- unlist(sumx_teisum_tei["tei"])
-        names(tei) <- colnames(es)
-        if (showprogress) {
+      OUT<-NULL
+      es<-NULL
+      for(i in seq_along(split_start)){
+        es<-ExpressionSet[,split_start[i]:split_end[i]]
+        es<-es[rownames(es) %in% common_ids,,drop=FALSE]
+        es<-es[order(rownames(es)), ]
+        sumx_teisum_tei<-rcpp_tei_parallel(es, Phylostratum, threads)
+        tei<-unlist(sumx_teisum_tei["tei"])
+        names(tei)<-colnames(es)
+        if(showprogress){
           utils::setTxtProgressBar(pb, i)
         }
         es <- NULL
-        OUT <- c(OUT, tei)
+        OUT<-c(OUT, tei)
       }
-      tei <- OUT
-    } else{
-      sumx_teisum_tei <- rcpp_tei_parallel(ExpressionSet, Phylostratum,
-                                           threads)
-      tei <- unlist(sumx_teisum_tei["tei"])
-      names(tei) <- colnames(ExpressionSet)
+      tei<-OUT
+    }else{
+      ExpressionSet<-ExpressionSet[rownames(ExpressionSet) %in%
+                                     common_ids,,drop=FALSE]
+      ExpressionSet<-ExpressionSet[order(rownames(ExpressionSet)), ]
+      sumx_teisum_tei<-rcpp_tei_parallel(ExpressionSet, Phylostratum)
+      tei<-unlist(sumx_teisum_tei["tei"])
+      names(tei)<-colnames(ExpressionSet)
+    }
+  }
+  if(is(ExpressionSet, "data.frame") | is(ExpressionSet, "tibble")){
+    if(is.ExpressionSet(ExpressionSet)){
+      Phylostratum<-setNames(ExpressionSet$Phylostratum,
+                             ExpressionSet$GeneID)
+      ExpressionSet<-as(data.matrix(
+        ExpressionSet[,3:ncol(ExpressionSet)]), "sparseMatrix")
+      rownames(ExpressionSet)<-names(Phylostratum)
+    }
+    if(ncol(ExpressionSet)>split){
+      split_start<-seq(from=1, to=ncol(ExpressionSet), by=split)
+      if(ncol(ExpressionSet)%%split != 0){
+        split_end<-c(seq(from=split, to=ncol(ExpressionSet),
+                         by=split), ncol(ExpressionSet))
+      }else{
+        split_end<-seq(from=split, to=ncol(ExpressionSet), by=split)
+      }
+      if(rev(split_end-split_start)[1]==0){
+        split_start<-split_start[-length(split_start)]
+        split_end[length(split_end)-1] <- rev(split_end)[1]
+        split_end<-split_end[-length(split_end)]
+      }
+      if(showprogress){
+        pb<-utils::txtProgressBar(min=1, max=length(split_start), style=3)
+      }
+      OUT<-NULL
+      es<-NULL
+      for(i in seq_along(split_start)){
+        es<-ExpressionSet[,split_start[i]:split_end[i]]
+        sumx_teisum_tei<-rcpp_tei_parallel(es, Phylostratum, threads)
+        tei<-unlist(sumx_teisum_tei["tei"])
+        names(tei)<-colnames(es)
+        if(showprogress){
+          utils::setTxtProgressBar(pb, i)
+        }
+        es<-NULL
+        OUT<-c(OUT, tei)
+      }
+      tei<-OUT
+    }else{
+      sumx_teisum_tei<-rcpp_tei_parallel(ExpressionSet, Phylostratum,
+                                         threads)
+      tei<-unlist(sumx_teisum_tei["tei"])
+      names(tei)<-colnames(ExpressionSet)
     }
   }
   return(tei)
