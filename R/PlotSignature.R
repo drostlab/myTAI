1b95d6d47ce55309addbef2bd29814069d4313eb
HajkD <hajkdrost@googlemail.com>
2023-07-09 15:47:42 +0200

    update message in PlotSignature()

--- R/PlotSignature.R
+++ R/PlotSignature.R
@@ -452,7 +452,7 @@ PlotSignature <-
         TI.ggplot <- TI.ggplot + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 1,hjust = 1))
         
         if (TestStatistic == "FlatLineTest")
-          message("Users can now investigate how good the permutation test performed by specifying the function: 'FlatLineTest(..., permutations  = ", permutations, ", plotHistogram = TRUE)'")
+          cat("Now run 'FlatLineTest(..., permutations  = ", permutations, ", plotHistogram = TRUE)' to analyse the permutation test performace.")
         return (TI.ggplot)
 }
 
