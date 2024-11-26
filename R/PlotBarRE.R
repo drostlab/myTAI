1973e03f7cbb01b064c458bed10bc1a55c86c54d
filipamc19 <48626872+filipamc19@users.noreply.github.com>
2024-11-26 09:40:47 +0000

    Refactoring the PlotBarRE function using ggplot2

--- R/PlotBarRE.R
+++ R/PlotBarRE.R
@@ -29,6 +29,8 @@
 #'
 #' (3) '***' = P-Value <= 0.0005  
 #' 
+#' (4) 'NS' = P-value > 0.05
+#' 
 #' @details 
 #' In case a large number of developmental stages is included in the input \code{ExpressionSet},
 #'  p-values returned by \code{PlotBarRE} should be adjusted for multiple comparisons which can be done
@@ -42,7 +44,7 @@
 #' 
 #' Myles Hollander and Douglas A. Wolfe (1973), Nonparametric Statistical Methods. New York: John Wiley & Sons. Pages 115-120.
 #' 
-#' @author Hajk-Georg Drost
+#' @author Hajk-Georg Drost and Filipa Martins Costa
 #' @seealso \code{\link{RE}}, \code{\link{REMatrix}},\code{\link{PlotRE}}, \code{\link{kruskal.test}}
 #' @examples
 #' 
@@ -76,154 +78,148 @@
 #' @export
 
 PlotBarRE <- function(ExpressionSet,
-                      Groups          = NULL,
-                      wLength         = 0.1,
-                      ratio           = FALSE,
-                      p.adjust.method = NULL, ...)
-{
-        
-        ExpressionSet <- as.data.frame(ExpressionSet)
-        is.ExpressionSet(ExpressionSet)
-        
-        if (is.null(Groups))
-                stop("Your Groups list does not store any items.", call. = FALSE)
-        
-        if (any(sapply(Groups,function(x) length(x) < 2)))
-                stop("Each Group class needs to store at least two items.", call. = FALSE)
-        
-        ### getting the PS names available in the given expression set
-        age_names <- as.character(names(table(ExpressionSet[ , 1])))
-        
-        # test whether all group elements are available in the age vector
-        ra <- range(ExpressionSet[ , 1])
-        if(!all(unlist(Groups) %in% as.numeric(age_names)))
-                stop("There are items in your Group elements that are not available in the age column of your ExpressionSet.", call. = FALSE)
-        
-        PS.Table <- age_names
-        nPS <- length(PS.Table)
-        PS.Names <- names(PS.Table)
-        nCols <- dim(ExpressionSet)[2]
-        nGroups <- length(Groups)
-        MeanREClassValues <- matrix(NA_real_,nGroups,nCols-2)
-        StdErr.RE.ClassValues <- matrix(NA_real_,nGroups,nCols-2)
-        pValues <- 1
-        AnovaListValues <- 1
-        barColors <- bar.colors(nGroups)
-        ### compute the relative expression profiles for all
-        ### given phylostrata
-        REmatrix <- matrix(NA_real_,ncol = nPS,nrow = nCols-2)
-        REmatrix <- age.apply(ExpressionSet = ExpressionSet, RE)
-        
-        ### compute the mean relative expression levels for each PS-Group
-        ### as well as the Std.Error of the relative expression levels
-        ### included in each PS-Group
-        for(i in 1:nGroups){
-                MeanREClassValues[i , ] <- colMeans(REmatrix[match(as.character(Groups[[i]]),
-                                                                   rownames(REmatrix)) , ])
-                
-                StdErr.RE.ClassValues[i , ] <- apply(REmatrix[match(as.character(Groups[[i]]),
-                                                                    rownames(REmatrix)) , ],2,std.error)
-        }   
-        
-        if(nGroups == 2){
-                for(j in 1:(nCols-2)){
-                        
-                        testForConstantValues <- try(stats::kruskal.test(list(REmatrix[match(as.character(Groups[[1]]),
-                                                                                      rownames(REmatrix)) , j],
-                                                                       REmatrix[match(as.character(Groups[[2]]),
-                                                                                      rownames(REmatrix)) , j])),
-                                                     silent = FALSE)
-                        
-                        if(methods::is(testForConstantValues, "try-error")){
-                                warning("Something went wrong with the Kruskal-Wallis Rank Sum Test... the p-value has been set to p = 1.", call. = FALSE)
-                                pValues[j] <- 1
-                        } 
-                        
-                        else
-                                pValues[j] <- testForConstantValues$p.value
-                        
-                }
-                
-                FoldChangeOfMeanREValues <- apply(MeanREClassValues,2,function(x){return((x[1]) / (x[2]))})
-                tmpFoldChanges <- FoldChangeOfMeanREValues
-                REFoldChangeOfMeanREValues <- (tmpFoldChanges - min(tmpFoldChanges)) / (max(tmpFoldChanges) - min(tmpFoldChanges))
-                
-        }
-        
-        if(nGroups > 2){
-                for(s in 1:(nCols-2)){
-                        for(k in 1:nGroups){
-                                AnovaListValues[k] <- list(REmatrix[match(as.character(Groups[[k]]), rownames(REmatrix)) , s])
-                        }
-                        
-                        pValues[s] <- stats::kruskal.test(AnovaListValues)$p.value
-                        
-                }
-        }
-        
-        if(!is.null(p.adjust.method)){
-                
-                pValues <- stats::p.adjust(pValues, method = p.adjust.method, n = nPS)
-        }
-        
-        pValNames <- rep("",nCols-2)
-        pValNames[which(pValues <= 0.05)] <- "*"
-        pValNames[which(pValues <= 0.005)] <- "**"
-        pValNames[which(pValues <= 0.0005)] <- "***"
-        pValNames[which(is.na(pValNames))] <- ""
-        
-        # define arguments for different graphics functions
-        barplot.args <- c("xlab","cex.lab","cex.axis","horiz","main","density","add","cex.names")
-        text.args <- c("cex")
-        dots <- list(...)
-        ellipsis.names <- names(dots)
-        
-        #print(pValues)
-        if(ratio == TRUE){
-                
-                REBarPlot <- do.call(graphics::barplot,c(list(MeanREClassValues,beside = TRUE,ylim = c(0,1),
-                                                              names.arg = names(ExpressionSet)[3:nCols],
-                                                              col = barColors,border = "white"),
-                                                         dots[!is.element(names(dots),c(text.args))]))
-                
-                do.call(graphics::text,c(list(apply(REBarPlot,2,mean),0.95,labels = pValNames),
-                                         dots[!is.element(names(dots),c(barplot.args))]))
-                
-                suppressWarnings(graphics::arrows(x0 = REBarPlot,y0 = ifelse(MeanREClassValues > 0,MeanREClassValues, (1/999)),x1 = REBarPlot,
-                       y1 = ifelse((StdErr.RE.ClassValues) == 0,MeanREClassValues + (1/999),
-                                   MeanREClassValues + StdErr.RE.ClassValues),code = 2, angle = 90, length = wLength))
-                
-                graphics::par(xpd = TRUE)
-                graphics::legend("topleft",
-                                 inset  = c(+0.2,0),
-                                 legend = paste("Group ",1:length(Groups),sep = ""),
-                                 fill   = barColors,
-                                 bty    = "n",
-                                 cex    = 1.3,
-                                 ncol   = ceiling(nGroups/2))
-                
-                graphics::par(xpd = FALSE)
-                if(nGroups == 2){
-                        graphics::lines(colMeans(REBarPlot), REFoldChangeOfMeanREValues,lty = 2,lwd = 5,col = "darkblue")
-                        do.call(graphics::axis,c(list(4,seq(0,1,0.2),format(seq(0,max(FoldChangeOfMeanREValues),length.out = 6),digits = 2)),
-                                                 dots[!is.element(names(dots),c(text.args))]))
-                }
-        }
-        
-        if(ratio == FALSE){
-                REBarPlot <- do.call(graphics::barplot,c(list(MeanREClassValues,beside = TRUE,ylim = c(0,1.6),
-                                                              names.arg = names(ExpressionSet)[3:nCols],col = barColors,border = "white"),
-                                                         dots[!is.element(names(dots),c(text.args))]))
-                
-                do.call(graphics::text,c(list(colMeans(REBarPlot),1.15,labels = pValNames),
-                                         dots[!is.element(names(dots),c(barplot.args))]))
-                
-                suppressWarnings(graphics::arrows(x0 = REBarPlot,y0 = ifelse(MeanREClassValues > 0,MeanREClassValues, (1/999)),x1 = REBarPlot,
-                       y1 = ifelse((StdErr.RE.ClassValues) == 0,MeanREClassValues + (1/999),
-                                   MeanREClassValues + StdErr.RE.ClassValues),code = 2, angle = 90, length = wLength))
-                
-                graphics::legend("topleft",legend = paste("Group ",1:length(Groups),sep = ""),fill = barColors,bty = "n",ncol = ceiling(nGroups/2))
-        }
-        
-}
+                      Groups = NULL,
+                      wLength = 0.1,
+                      ratio = FALSE,
+                      p.adjust.method = NULL, ...) {
+  
+  ExpressionSet <- as.data.frame(ExpressionSet)
+  myTAI::is.ExpressionSet(ExpressionSet)
+  
+  if (is.null(Groups))
+    stop("Your Groups list does not store any items.", call. = FALSE)
+  
+  if (any(sapply(Groups, function(x) length(x) < 2)))
+    stop("Each Group class needs to store at least two items.", call. = FALSE)
+  
+  # Define standard error function
+  std.error <- function(x) {
+    sd(x, na.rm = TRUE) / sqrt(length(x))
+  }
+  
+  # getting the PS names available in the given expression set
+  age_names <- as.character(names(table(ExpressionSet[, 1])))
+  
+  # test whether all group elements are available in the age vector
+  ra <- range(ExpressionSet[, 1])
+  if (!all(unlist(Groups) %in% as.numeric(age_names)))
+    stop("There are items in your Group elements that are not available in the age column of your ExpressionSet.", call. = FALSE)
+  
+  PS.Table <- age_names
+  nPS <- length(PS.Table)
+  PS.Names <- names(PS.Table)
+  nCols <- dim(ExpressionSet)[2]
+  nGroups <- length(Groups)
+  MeanREClassValues <- matrix(NA_real_, nGroups, nCols - 2)
+  StdErr.RE.ClassValues <- matrix(NA_real_, nGroups, nCols - 2)
+  pValues <- 1
+  AnovaListValues <- 1
+  
+  # compute the colors of the bars
+  if (nGroups < 3) {
+    barColors <- RColorBrewer::brewer.pal(3, "Set1")  
+  } else {
+    barColors <- RColorBrewer::brewer.pal(nGroups, "Set1")  
+  }
+  
+  ### compute the relative expression profiles for all
+  ### given phylostrata
+  REmatrix <- matrix(NA_real_, ncol = nPS, nrow = nCols - 2)
+  REmatrix <- myTAI::age.apply(ExpressionSet = ExpressionSet, RE)
+  
+  ### compute the mean relative expression levels for each PS-Group
+  ### as well as the Std.Error of the relative expression levels
+  ### included in each PS-Group
+  for (i in 1:nGroups) {
+    MeanREClassValues[i, ] <- colMeans(REmatrix[match(as.character(Groups[[i]]),
+                                                      rownames(REmatrix)), ])
+    
+    StdErr.RE.ClassValues[i, ] <- apply(REmatrix[match(as.character(Groups[[i]]),
+                                                       rownames(REmatrix)), ], 2, std.error)
+  }
+  
+  if (nGroups == 2) {
+    for (j in 1:(nCols - 2)) {
+      testForConstantValues <- try(stats::kruskal.test(list(REmatrix[match(as.character(Groups[[1]]),
+                                                                           rownames(REmatrix)), j],
+                                                            REmatrix[match(as.character(Groups[[2]]),
+                                                                           rownames(REmatrix)), j])),
+                                   silent = FALSE)
+      
+      if (methods::is(testForConstantValues, "try-error")) {
+        warning("Something went wrong with the Kruskal-Wallis Rank Sum Test... the p-value has been set to p = 1.", call. = FALSE)
+        pValues[j] <- 1
+      } else {
+        pValues[j] <- testForConstantValues$p.value
+      }
+    }
+    
+    FoldChangeOfMeanREValues <- apply(MeanREClassValues, 2, function(x) { return((x[1]) / (x[2])) })
+    tmpFoldChanges <- FoldChangeOfMeanREValues
+    REFoldChangeOfMeanREValues <- (tmpFoldChanges - min(tmpFoldChanges)) / (max(tmpFoldChanges) - min(tmpFoldChanges))
+  }
+  
+  if (nGroups > 2) {
+    for (s in 1:(nCols - 2)) {
+      for (k in 1:nGroups) {
+        AnovaListValues[k] <- list(REmatrix[match(as.character(Groups[[k]]), rownames(REmatrix)), s])
+      }
+      
+      pValues[s] <- stats::kruskal.test(AnovaListValues)$p.value
+    }
+  }
+  
+  if (!is.null(p.adjust.method)) {
+    pValues <- stats::p.adjust(pValues, method = p.adjust.method, n = nPS)
+  }
+  
+  # add significance stars to the labels based on p-values
+  pValNames <- rep("", nCols - 2)
+  pValNames[which(pValues > 0.05)] <- "NS"
+  pValNames[which(pValues <= 0.05)] <- "*"
+  pValNames[which(pValues <= 0.005)] <- "**"
+  pValNames[which(pValues <= 0.0005)] <- "***"
+  pValNames[which(is.na(pValNames))] <- ""
+  
+  # define the stages
+  stages <- colnames(ExpressionSet[ , 3:ncol(ExpressionSet)])
+  
+  # convert the MeanREClassValues matrix into a long format data frame for ggplot2
+  df <- data.frame(
+    stage = rep(1:ncol(MeanREClassValues), each = nrow(MeanREClassValues)),
+    mean = as.vector(MeanREClassValues),
+    error = as.vector(StdErr.RE.ClassValues),
+    group = rep(1:nrow(MeanREClassValues), times = ncol(MeanREClassValues))
+  )
+  
+  # calculate position for the significance bars
+  xmin_pvalue <- seq(from = 0.8, by = 1, length(stages))
+  xmax_pvalue <- seq(from = 1.2, by = 1, length(stages) + 1)
+  y_position <- tapply(df$mean + df$error, df$stage, max) + 0.15
+  y_position <- as.numeric(y_position)
+  
+  groups <- c()
+  for (group in Groups){
+    groups <- c(groups, paste(group[1], "-", tail(group,1)))
+  }
 
+  # create the ggplot
+  p <- ggplot2::ggplot(df, ggplot2::aes(x = factor(stage), y = mean, fill = as.factor(group))) +
+    ggplot2::geom_bar(stat = "identity", position = "dodge", color = "black", width = 0.7) + 
+    ggsignif::geom_signif(xmin = xmin_pvalue, xmax = xmax_pvalue, y_position = y_position ,annotation = pValNames) +
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = mean - error, ymax = mean + error), width = 0.25, position = ggplot2::position_dodge(width = 0.7)) + 
+    ggplot2::theme_minimal(base_size = 15) + 
+    ggplot2::labs(y = "Mean Relative Expression", x = "Stage") + 
+    ggplot2::scale_fill_manual(values = barColors, labels = groups) + 
+    ggplot2::scale_x_discrete(labels = stages) +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(angle = 45, hjust = 1),
+      axis.title.x = ggplot2::element_text(size = 16),
+      axis.title.y = ggplot2::element_text(size = 16),
+      legend.title = ggplot2::element_blank(),
+      legend.position = "top",
+      legend.key = ggplot2::element_rect(fill = "white")
+    )
+  
+  print(p)
+}
