e5d8e6316a9523d83a7f0e2bae99866efd8795c0
nifets <stefanmanol@gmail.com>
2025-04-29 17:18:06 +0100

    plot contribution

---
+++ R/plot_contribution.R
@@ -0,0 +1,22 @@
+
+#' @import dplyr ggplot2
+plot_contribution <- function(phyex_set) {
+    contribution <- sTXI(phyex_set, option="identity")
+    df <- data.frame(contribution) |>
+        mutate(PS = factor(row_number())) |>
+        tidyr::pivot_longer(-PS, names_to = "Condition", values_to = "TXI") |>
+        mutate(Condition = factor(Condition, levels=unique(Condition))) |>
+        mutate(PS = forcats::fct_rev(PS)) # reverse PS order
+    
+    
+    p <- ggplot(df, 
+                aes(x = Condition, 
+                    y = TXI, 
+                    fill = PS, 
+                    group = PS)) +
+        geom_area(position = "stack", colour="black", size=0.2) +
+        labs(x=phyex_set@conditions_label, y=phyex_set@index_full_name) +
+        scale_fill_manual(values = PS_colours(nrow(contribution))) +
+        theme_minimal()
+    return(p)
+}
