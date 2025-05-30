0c7504510bf8aea684a842694a8f5e5740252503
nifets <stefanmanol@gmail.com>
2025-05-30 17:40:55 +0100

    faster null txis

--- R/plot_strata_expression.R
+++ R/plot_strata_expression.R
@@ -9,10 +9,32 @@ plot_strata_expression <- function(phyex_set,
         mutate(Expression = apply(phyex_set@count_matrix, 1, aggregate_FUN))
     
     p <- ggplot(df, aes(x=Stratum, y=Expression, colour=Stratum)) +
-        geom_boxplot() + 
+        geom_jitter() + 
         scale_colour_manual(values = PS_colours(phyex_set@num_strata)) +
         labs(y=paste("Expresssion aggregated by", agg_name)) + 
         guides(colour="none") +
+        theme_minimal()
+    
+    return(p)
+}
+
+#' @import ggplot2 
+plot_strata_expression_rank <- function(phyex_set,
+                                   aggregate_FUN = mean) {
+    
+    agg_name <- deparse(substitute(aggregate_FUN))
+    aggregate_FUN <- match.fun(aggregate_FUN)
+    df <- phyex_set@data |>
+        mutate(Expression = apply(phyex_set@count_matrix, 1, aggregate_FUN)) |>
+        arrange(Expression) |>
+        mutate(Rank = row_number()) |>
+        mutate(PS_num = as.numeric(Stratum)) |>
+        mutate(AvgPS = zoo::rollapply(PS_num, width=20, FUN=mean, align= "center", fill="extend"))
+    
+    p <- ggplot(df, aes(x=Rank, y=Expression, colour=AvgPS), alpha=0.3) +
+        geom_point() + 
+        scale_colour_gradient(low = "white", high = "blue") +
+        labs(y=paste("Expresssion aggregated by", agg_name)) + 
         theme_minimal()
     
     return(p)
--- src/null_txis.cpp
+++ src/null_txis.cpp
@@ -1,61 +1,27 @@
+#include <RcppArmadillo/Lighter>
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppThread.h>
 
-#include <random>
-#include <iostream>
-#include <algorithm>
 
-#include <RcppParallel.h>
-#include <RcppEigen.h>
-#include <Eigen/Dense>
-#include <Eigen/Core>
-// [[Rcpp::depends(RcppEigen)]]
-// [[Rcpp::depends(RcppParallel)]]
-// [[Rcpp::plugins(cpp11)]]
 
-using namespace Rcpp;
-using namespace RcppParallel;
-
-struct PermuteWorker : public Worker {
-    std::vector<double>& vec;
-    Eigen::MatrixXd& mat;
-    std::mt19937 g;
-    
-    PermuteWorker(std::vector<double>& vec_, Eigen::MatrixXd& mat_)
-        : vec(vec_), mat(mat_), g(std::random_device{}()) {}
-    
-    void operator()(std::size_t begin, std::size_t end) {
-        std::vector<double> local_vec = vec;
-        
-        for (std::size_t i = begin; i < end; i++) {
-            std::shuffle(local_vec.begin(), local_vec.end(), g);
-            Eigen::Map<Eigen::VectorXd> tmp_vec(local_vec.data(), local_vec.size());
-            mat.col(i) = tmp_vec;
-        }
-    }
-};
-
-Eigen::MatrixXd permute_mat_parallel(Eigen::VectorXd vector, int num_permutations) {
-    std::vector<double> vec(vector.data(), vector.data() + vector.size());
-    Eigen::MatrixXd mat(vec.size(), num_permutations);
-    PermuteWorker worker(vec, mat);
-    parallelFor(0, mat.cols(), worker);
-    return mat;
-}
-
+//credit to Filipa Martins Costa
 
 // [[Rcpp::export]]
-Eigen::MatrixXd cpp_nullTXIs(Eigen::MatrixXd count_matrix, Eigen::VectorXd strata_vector, int num_permutations){
-    // Normalise expression sample-wise
-    Eigen::MatrixXd matrix_norm = count_matrix.array().rowwise() / count_matrix.colwise().sum().array();
+arma::mat cpp_nullTXIs(const arma::mat& count_matrix, const arma::vec& strata_vector, int num_permutations) {
+    int m = count_matrix.n_cols;
+
+    arma::mat txis(num_permutations, m, arma::fill::zeros);
+    
+    RcppThread::ProgressBar bar(num_permutations, 1);
+    
+    arma::mat count_norm = count_matrix.each_row() / sum(count_matrix, 0);
+    
+    RcppThread::parallelFor(0, num_permutations, [&](int i) {
+        arma::vec perm = shuffle(strata_vector);
+        txis.row(i) = sum(count_norm.each_col() % perm, 0);
+        bar++;
+    });
     
     
-    Eigen::MatrixXd perm_mat = permute_mat_parallel(strata_vector, num_permutations);
-    Eigen::MatrixXd res = matrix_norm.transpose() * perm_mat;
-    return res.transpose();
+    return txis;
 }
-
-
-
-
-
-
-
