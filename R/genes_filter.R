491c0b46b0cc7d62d17838251f51bdc015c209b1
nifets <stefanmanol@gmail.com>
2025-07-27 17:03:00 +0100

    fixed test

--- R/genes_filter.R
+++ R/genes_filter.R
@@ -33,6 +33,13 @@
 genes_top_expr <- function(phyex_set, FUN = rowMeans, p = .99, ...) {
     metric_values <- FUN(phyex_set@expression, ...)
     names(metric_values) <- phyex_set@gene_ids
+
+    if (p >= 1) {
+        return(character(0)) # Return no genes if p = 1
+    }
+    if (p <= 0) {
+        return(names(metric_values)) # Return all genes if p = 0
+    }
     
     top_genes <- names(metric_values)[metric_values > stats::quantile(metric_values, p, na.rm = TRUE)]
     
