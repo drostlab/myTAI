5826fa644ab2866f7c570e90eaab7bc221616c42
nifets <stefanmanol@gmail.com>
2025-08-27 20:06:42 +0300

    fixed select genes bug, made plot distribution strata better

--- R/plot_distribution_strata.R
+++ R/plot_distribution_strata.R
@@ -33,23 +33,26 @@ plot_distribution_strata <- function(strata,
                                      as_log_obs_exp = FALSE) {
 
     if (!is.factor(strata)) {
-        stop("'strata' must be a factor vector.", call. = FALSE)
+        stop("'strata' must be a factor vector. Try providing phyex_set@strata as input.", call. = FALSE)
     }
 
     df <- data.frame(Stratum=strata, GeneID=names(strata))
     df_selected <- df |> filter(GeneID %in% selected_gene_ids)
     if (!as_log_obs_exp) {
-        ggplot(df_selected, aes(Stratum, fill=Stratum)) + 
-            geom_bar(stat="count", position = "dodge", show.legend = TRUE) + 
+        ggplot(df_selected, aes(y = Stratum, fill=Stratum)) + 
+            geom_bar(stat="count", position = "dodge", show.legend = FALSE) + 
             labs(fill="Stratum") +
-            xlab("Stratum") + 
-            ylab("Gene Count") +
-            scale_x_discrete(labels=labels(df$Stratum), drop = FALSE) +
+            ylab("Stratum") + 
+            xlab("Gene Count") +
+            scale_y_discrete(labels=levels(df$Stratum), drop = FALSE) +
             scale_fill_manual(values = PS_colours(length(levels(strata))), labels=levels(df_selected$Stratum), drop=FALSE) +
             guides(fill=guide_legend(override.aes = list(size = 1.0), keyheight = 0.5, keywidth = 0.5)) +
             theme_minimal() +
-            theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
-            geom_text(stat = "count", aes(label = after_stat(count)), vjust = -0.5, size=3)
+            theme(axis.text.y = element_text(angle = 0, hjust = 1), 
+                  axis.text.x = element_blank(), 
+                  axis.ticks.x = element_blank()) +
+            geom_text(stat = "count", aes(label = after_stat(count)), hjust = -0.1, size=3)+
+            coord_cartesian(clip = "off")
     }
     else {
         counts_all <- df |> count(Stratum) |> mutate(p_all = n / sum(n))
@@ -57,16 +60,17 @@ plot_distribution_strata <- function(strata,
         log_ratio <- full_join(counts_all, counts_sel, by= "Stratum") |>
             mutate(log_obs_exp = log2((p_sel + 1e-6)/ (p_all + 1e-6)))
         
-        ggplot(log_ratio, aes(Stratum, 
-                              y=log_obs_exp,
+        ggplot(log_ratio, aes(y=Stratum, 
+                              x=log_obs_exp,
                               fill= log_obs_exp)) +
             geom_bar(stat = "identity", size=0.1) +
             labs(fill="Stratum") +
-            xlab("Stratum") +
-            ylab("Log(Obs/Exp)") +
-            scale_x_discrete(labels=labels(df$Stratum), drop = FALSE) +
+            ylab("Stratum") +
+            xlab("Log(Obs/Exp)") +
+            scale_y_discrete(labels=levels(df$Stratum), drop = FALSE) +
             scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
             guides(fill="none") + 
+            theme(axis.text.y = element_text(angle = 0, hjust = 1)) +
             theme_minimal()
     
         
